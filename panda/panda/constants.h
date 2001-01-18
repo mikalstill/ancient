e6d69f968daff94b2d7d0a61e8638efaf620ea59
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2001-01-18 00:58:42 +0000

    Mikal: YANF

--- panda/panda/constants.h
+++ panda/panda/constants.h
@@ -80,9 +80,9 @@ enum
 // The image types supported
 enum
 {
-  gImageTiff = 0,
-  gImageJpeg,
-  gImagePNG
+  panda_image_tiff = 0,
+  panda_image_jpeg,
+  panda_image_png
 };
 
 #ifdef __cplusplus
