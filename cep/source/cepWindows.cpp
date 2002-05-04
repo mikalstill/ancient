4388c65e8a763853353224b0225be41fae720a47
u991672 <u991672@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-05-04 04:26:07 +0000

    Tidied up previous edits. Fixed indeces in array/vector references in loops and else where Began implemenation of vector for data and window array. Began implementation of return type for mulitple return paramater to be done in the form of return cepDataset populated with relavent information. This implementation is incomplete. Daniel

--- cep/source/cepWindows.cpp
+++ cep/source/cepWindows.cpp
@@ -1,7 +1,7 @@
 /*
 
  These methods will be placed into cepDataset .....soon.
- 
+ Vector not yet iMPLEMENTED FOR WINDOWaRRAY.	or data.
 
 */
 
@@ -9,13 +9,9 @@
 //doto_daniel: change the indices for array and vectors so they start at ZERO not ONE
 
 void //todo_daniel: this will end being a return type .
-cepDataset::doHam (double data, double startWindow, double winSize)
+cepDataset::doHam (double startWindow, double winSize)
 {
-	/*
-		todo_daniel: find out where this will be called from...called after doWindow or from doWindow 
-		Gets a single hamming window value
-		Imports:
-		* data: a data element from the standard array of data [date,value,error]
+	/*	Imports:
 		* startWindow: the date of the start of the hamming window
 		* winSize: the size (in decimal years) of the window
 
@@ -23,36 +19,35 @@ cepDataset::doHam (double data, double startWindow, double winSize)
 		* value: the hamming value
 		* weight: the weight of the value within the set
 		todo_daniel: again here with two return types...maybe have them as members of the class with get and set	
-		 
+		
+		Uses m_data: a data element from the standard array of data [date,value,error]
 	*/
-
+	double weight=0;
+	double value=0;
 	weight = 0.54 - 0.46*cos(2*pi*((data(1)-startWindow)/(winSize)); //todo:daniel ask nick why 0.54 and .46...shouldn't be hard coded here.
 	value  = data(2) * weight;
+	cepDataset ham =  new cepDataset();
+	return ham;
 
 }//end doHam
 
 void		//this will be a method in cepDataset -- will need to include math.h -- will most likely have a return type.
-cepDataset::doWindow (double data[obs][2],double winSize, double overlap) //todo_daniel: once vector exists..fix this..wont need param data.
+cepDataset::doWindow (double winSize, double overlap) //todo_daniel: once vector exists..fix this..wont need param data.
 {
 	/*
 	Groups data into square windows with a pre-defined width. 
 	Imports: 
-	* data - matrix (observation x 3 - date, value, error) todo_daniel:vector
 	* winSize - size of each window (deciaml years)
 	* overlap - amount of overlap between each window
 
 	Exports:
-	* windowArray: Array of windowed data (3 * numWindows * largestWindow) todo_daniel: maybe vector here too!
+	* windowArray: Array of windowed data (3 X numWindows X largestWindow) todo_daniel: maybe vector here too!
 	* numWindows: The number of seperate windows that were populated with data
-	
-	//todo_daniel: return 2 params? or maybe calculate numWindows in calling method?
-	//todo_daniel: or maybe just call doHam from here sending params: startWindow and winSize
-
 	*/
 
-	int numSample; //number of smaples in the vector
-	int nextFirstRecord; //
-	int currentFirstRecord; //
+	int numSamples; //number of smaples in the vector
+	int nextFirstRecord; //start of next window
+	int currentFirstRecord; //start of current window
 	int numWindows; //the total number  of windows required
 	int vecCtr; //vector counter
 	double firstDate; //the first dat in the the data
@@ -60,9 +55,9 @@ cepDataset::doWindow (double data[obs][2],double winSize, double overlap) //todo
 	double overlapWinSize; //
 	
   // get timescale of data set 
-  numSamples = sizeOf TheDataVector //length(data(1,:));//todo_daniel: correct notation for vector size...vector not defined yet
-  firstdate = todo_daniel:vector[1][1]; //data(1,1);//todo_daniel: vector not defined yet..correct notation needed
-  lastdate = todo_daniel:vector[1][numSamples];//todo_daniel: vector not defined yet..correct notation needed
+  numSamples = m_data.size() //length(data(1,:));//todo_daniel: correct notation for vector size...vector not defined yet
+  firstdate = m_data[0][0]; //data(1,1);//todo_daniel: vector not defined yet..correct notation needed
+  lastdate = m_data[numSample][0];//todo_daniel: vector not defined yet..correct notation needed
 
   // For optimizing speed slightly 
   overlapWinSize = winSize * (1-overlap);
@@ -74,10 +69,10 @@ cepDataset::doWindow (double data[obs][2],double winSize, double overlap) //todo
 	}
   
   // Divide into windows 
-  nextFirstRecord = 1;
-  startWindow = todo_daniel:vector[1][1] - winSize; //totdo_daniel: vector notation
-  for (int i=1; numWindows-1; i++){
-	  // point vector counter to start of window 
+  nextFirstRecord = 0;
+  startWindow = m_data[0][0] - winSize; 
+  for (int i=0; numWindows; i++){
+	// point vector counter to start of window 
     vecCtr = nextFirstRecord;
     currentFirstRecord = nextFirstRecord;
 
@@ -85,9 +80,9 @@ cepDataset::doWindow (double data[obs][2],double winSize, double overlap) //todo
     startWindow = startWindow + overlapWinSize;
 
     // populate first half of window
-    while (todo_daniel:vector[1][vecCtr] < (startWindow+overlapWinSize)){
-        for (int k=1; k<=3; k++){
-            windowArray(k,i,vecCtr-(currentFirstRecord-1)) = todo_daniel:vector[k][vecCtr];
+    while (m_data[vecCtr][0] < (startWindow+overlapWinSize)){
+        for (int k=0; k<3; k++){
+            windowArray(k,i,vecCtr-(currentFirstRecord)) = m_data[vecCtr][k];
         }
         vecCtr += 1; // increment vector counter
 		} //end while
@@ -96,24 +91,27 @@ cepDataset::doWindow (double data[obs][2],double winSize, double overlap) //todo
     nextFirstRecord = vecCtr;
 
     // populate second half of window
-    while (todo_daniel:vector[1][vecCtr] < (startWindow+winSize)){
-        for (k=1; k<=3; k++){
-            windowArray(k,i,vecCtr-(currentFirstRecord-1)) = todo_daniel:vector[k][vecCtr];
+    while m_data[vecCtr][0] < (startWindow+winSize)){
+        for (k=0; k<3; k++){
+            windowArray(k,i,vecCtr-(currentFirstRecord)) = m_data[vecCtr][k];
         }
         vecCtr += 1; // increment vector counter
 		} //end while
   } // end for
 
   // populate final window with remaining data
-  vecCtr = nextFirstRecord;
+  vecCtr = nextFirstRecord; 
   currentFirstRecord = nextFirstRecord;
   startWindow = startWindow + winSize;
   while (vecCtr < numSamples){
-    for (k=1; k<=3; k++){
-      windowArray(k,numWindows,vecCtr-(currentFirstRecord-1)) = todo_daniel:vector[k][vecCtr];
+    for (k=0; k<3; k++){
+      windowArray(k,numWindows,vecCtr-(currentFirstRecord)) = m_data[vecCtr][k];
     }
     vecCtr += 1;
-	} //end while
+  } //end while
+
+  cepDatset windowData = new cepDataset(windowArray, numWindows);
+  return windowData;
 
 }//end void cepDatset::doWindow
 
