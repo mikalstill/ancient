e9cbaf300f5eb5a3d134c69fe0b960738da45b90
u982087 <u982087@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-05-19 08:18:45 +0000

    Cleaned up interface and began implementing a file browser to allow searching for data files.

--- cep/prototype/cep_callback.m
+++ cep/prototype/cep_callback.m
@@ -16,3 +16,137 @@ function f = cep_callback( object )
 % OUTPUTS:
 %       none.
 %
+
+
+
+
+% ==============================================================
+switch( object )
+case 'Exit'
+    close;
+case 'Load'
+    filename = get_filename;
+end
+
+
+
+% ==============================================================
+%                gets the filenmame from the user
+% ==============================================================
+function name = get_filename
+%
+%
+%
+screen_size = get(0,'ScreenSize');
+
+load_width=screen_size(3)/4;
+load_height=screen_size(4)/4;
+load_pos = [ (screen_size(3)-load_width)/2, (screen_size(4)-load_height)/2, load_width, load_height ];
+load_handle = figure('Units','pixels','Color',[0.8 0.8 0.8],'Position',load_pos,'Tag','Select a File');
+set(load_handle, 'WindowStyle', 'modal' );
+% set(load_handle,'MenuBar','none');
+
+sites = get_files;
+dirs = get_dirs;
+
+space = 10;
+dirbox_pos = [ space, space, load_width/2-space, load_height-(2*space) ];
+dirbox_handle = uicontrol('Parent',load_handle,'Units','pixels','BackgroundColor',[1.0 1.0 1.0],'Position',dirbox_pos,'String',dirs,'Style','listbox','Tag','Filebox','Value',1,'UserData',dirs);
+
+filebox_pos = [ load_width/2+space/2, space, load_width/2-space, load_height-(2*space) ];
+filebox_handle = uicontrol('Parent',load_handle,'Units','pixels','BackgroundColor',[1.0 1.0 1.0],'Position',filebox_pos,'String',sites,'Style','listbox','Tag','Dirbox','Value',1,'UserData',sites);
+
+% return something
+name='blah';
+
+
+
+% ==============================================================
+%                gets a listing of the current directory
+% ==============================================================
+function files = get_files
+% function list = get_files
+%
+% gets a listing for the current working directory.
+%
+% this code is based on the 'tsview' from MIT.
+%
+% INPUTS:
+%       cwd - the directory to get the listing of
+% OUTPUTS
+%       list - the directory listing
+% PRE:
+%       none
+% POST:
+%       none
+%
+
+% get the directory listing
+listing = dir;
+
+names = {listing.name};
+% get all file names with mb_ in the first three characters
+names = names(strncmp(names,'mb_',3));
+% Now sort the names
+names = sort(names)';
+
+% Reduce the names further by getting only the site names
+list = [];
+for i = 1:length(names)
+	tname = cell2struct(names(i),'name');
+	if findstr(tname.name,'.dat1')
+		st = tname.name;
+		list = [list;st(4:11)];
+	end
+end
+files=list;
+
+
+
+% ==============================================================
+%                gets a listing of the current directory
+% ==============================================================
+function dirs = get_dirs
+% function list = get_dirs
+%
+% gets a listing of directories only for the current working
+% directory.
+%
+% this code is based on the 'tsview' from MIT.
+%
+% INPUTS:
+%       cwd - the directory to get the listing of
+% OUTPUTS
+%       list - the directory listing
+% PRE:
+%       none
+% POST:
+%       none
+%
+
+% get the directory listing
+listing = dir;
+
+% Isolate the directories
+j=0; dirlist=[];
+for i = 1:length(listing)
+    if listing(i).isdir
+        dirlist = [dirlist;listing(i)];
+    end
+end
+
+%get their names
+names = { dirlist.name };
+list = [];
+%for i = 1:length(names)
+%    tname = cell2struct(names(i),'name');
+%    st = tname.name;
+%    % TODO... require a fixed size filename to keep name length consistency
+%    list = [list;st];
+%end
+
+tname = cell2struct(names(3),'name');
+st = tname.name;
+list = [st];
+
+dirs=list;
--- cep/prototype/cep_gui.m
+++ cep/prototype/cep_gui.m
@@ -1,6 +1,6 @@
-function gui = cps_gui()
+function gui = cep_gui()
 %
-% function gui = cps_gui()
+% function gui = cep_gui()
 %
 % Displays an interface to permit the viewing of geodetic data
 %
@@ -36,17 +36,18 @@ top_border_width = 10;
 frame_pos = [15,70,screen_size(3)-30,screen_size(4)-150];
 % pos = [16 67 1120 734]
 gui_handle = figure('Units','pixels','Color',[0.8 0.8 0.8],'Position',frame_pos,'Tag','Fig1');
+set(gui_handle,'ToolBar','figure');
 
-%number of buttons
+% number of buttons
 button_count = 2;
 x_location = 40;
 button_width = 100;
 button_height = 35;
-button_space = (frame_pos(4) - (button_count*button_height))/(button_count+1)
+button_space = (frame_pos(4) - (button_count*button_height))/(button_count+1);
 
 % Load button
 load_pos = [x_location, frame_pos(4)-button_space, button_width, button_height];
-load_handle = uicontrol('Parent',gui_handle, 'Callback','cep_callback(''Load'')', 'Units','pixels','Position',load_pos, 'String','Append','Tag','load_button','TooltipString','Load data from file');
+load_handle = uicontrol('Parent',gui_handle, 'Callback','cep_callback(''Load'')', 'Units','pixels','Position',load_pos, 'String','Load','Tag','load_button','TooltipString','Load data from file');
 
 % Exit button
 exit_pos = [x_location, frame_pos(4)-((2*button_space)+button_height), button_width, button_height];
@@ -58,18 +59,18 @@ axis_space = 60;
 axis_width  = 2*screen_size(3)/3;
 axis_height = screen_size(4)/4;
 
-pos = [border_width + (screen_size(3)/3), top_border_width + (axis_space/3), axis_width, axis_height];
-h1 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
+pos = [border_width + (screen_size(3)/5), top_border_width + (axis_space/3), axis_width, axis_height];
+axis1 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
     'Color',[1 1 1], 'Position',pos, 'Tag','AxisNorth', ...
     'XColor',[0 0 0], 'YColor',[0 0 0], 'ZColor',[0 0 0]);
 
-pos = [border_width + (screen_size(3)/3), top_border_width + (axis_height+(2*axis_space/3)), axis_width, axis_height];
-h2 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
+pos = [border_width + (screen_size(3)/5), top_border_width + (axis_height+(2*axis_space/3)), axis_width, axis_height];
+axis2 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
     'Color',[1 1 1], 'Position',pos, 'Tag','AxisEast', ...
     'XColor',[0 0 0], 'YColor',[0 0 0], 'ZColor',[0 0 0]);
 
-pos = [border_width + (screen_size(3)/3), top_border_width + ((2*axis_height)+(axis_space)), axis_width, axis_height];
-h3 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
+pos = [border_width + (screen_size(3)/5), top_border_width + ((2*axis_height)+(axis_space)), axis_width, axis_height];
+axis3 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
     'Color',[1 1 1], 'Position',pos, 'Tag','AxisUp', ...
     'XColor',[0 0 0], 'YColor',[0 0 0], 'ZColor',[0 0 0]);
 
