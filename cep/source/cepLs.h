241c8e7783ed079188c5628f922c06c12350dee1
u983118 <u983118@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-06-02 05:37:39 +0000

    Added cepLs.cpp cepLs.h - this stuff is to do the linear least squares regression on the data. I still need to fix some stuff to do with optimizing matrix multiplications. I also need to intergrate it with cepDataset stuff.

---
+++ cep/source/cepLs.h
@@ -0,0 +1,34 @@
+#include <iostream.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct cepMatrix
+{
+  double* matrix;
+  int numRows;
+  int numCols;
+} blah;
+
+class cepLs
+{
+public:
+  
+  cepLs();
+  ~cepLs();
+
+  void cepDoLeastSquares(); 
+  
+private:
+  cepMatrix matA, matP, matL;
+  void sanityCheck( cepMatrix &matA, cepMatrix &matP, cepMatrix &matL);
+  bool isDiagonal( cepMatrix &mat );
+  
+  cepMatrix inverse( cepMatrix &mat );
+  cepMatrix transpose( cepMatrix &mat );
+  cepMatrix mulDiag( cepMatrix &matA, cepMatrix &matB );
+  cepMatrix mul( double* row, int rowElements, cepMatrix &mat );
+
+};
+  
+  
+  
