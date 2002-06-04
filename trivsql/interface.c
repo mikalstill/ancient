e69258b0aa1430dcf32b65f8a36d0c2ffaa2e382
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-06-04 07:02:56 +0000

    Mikal: About to implement bison input functions...

--- trivsql/interface.c
+++ trivsql/interface.c
@@ -1,12 +1,20 @@
 #include "trivsql.h"
 
+char *gTrivData = NULL;
+int gTrivInset;
+
 // Interface to the SQL database
 trivsql_state *trivsql_opendb(char *path){
   return trivsql_init(path);
 }
 
 trivsql_recordset *trivsql_execute(trivsql_state *state, char *sql){
-  printf("Executing...\n");
+  trivsql_xfree(gTrivData);
+  gTrivData = trivsql_xsnprintf("%s", sql);
+  gTrivInset = 0;
+}
 
+int trivsql_gettext(char *buffer, int maxlen){
+  
 
 }
