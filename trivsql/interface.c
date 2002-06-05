584ba2628caa314ebb3d8919e36f2ed2b3e917fe
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-06-05 03:26:19 +0000

    Mikal: Started work on the recordset control functions

--- trivsql/interface.c
+++ trivsql/interface.c
@@ -38,54 +38,19 @@ int trivsql_gettext(char *buffer, int maxlen){
 
 void trivsql_displayrs(trivsql_recordset *rs){
   int i, col;
-  char *t, *u, *c, *localCols, *tempCols;
+  char *t, *u, *c, *localCols;
   trivsql_row *theRow;
   trivsql_col *theCol;
 
   // Print the header line
-  printf("Select returned %d rows of %d columns\n\n=", rs->numRows, rs->numCols);
+  printf("\n=");
   for(i = 0; i < rs->numCols; i++){
     printf("===============");
   }
   printf("\n|");
 
-  // If the columns list is '*', substitute a list of all the columns
-  if(strcmp(rs->cols, "*") == 0)
-    localCols = trivsql_getallcolumns(rs->tname);
-  else{
-    localCols = rs->cols;
-    tempCols = trivsql_xsnprintf("%s", rs->cols);
-
-    // Determine that the named columns exist (unless we know they do)
-    col = 0;
-    c = strtok(tempCols, ";");
-    while(c != NULL){
-      i = 0;
-      while(1){
-	t = trivsql_xsnprintf("trivsql_%s_col%d", rs->tname, i);
-	u = trivsql_dbread(gState, t);
-	
-	if(u == NULL){
-	  trivsql_xfree(t);
-	  fprintf(stderr, "%s is an unknown column\n", c);
-	  return;
-	}
-	else if(strcmp(u, c) == 0){
-	  trivsql_xfree(t);
-	  trivsql_xfree(u);
-	  break;
-	}
-	
-	trivsql_xfree(t);
-	trivsql_xfree(u);
-	i++;
-      }
-
-    c = strtok(NULL, ";");
-    }
-  }
-
   // Print out the column names
+  localCols = trivsql_xsnprintf("%s", rs->cols);
   c = strtok(localCols, ";");
   while(c != NULL){
     printf(" %-12s |", c);
@@ -117,4 +82,6 @@ void trivsql_displayrs(trivsql_recordset *rs){
   }
 
   printf("\n\n");
+  printf("Select returned %d rows of %d columns\n\n", 
+	 rs->numRows, rs->numCols);
 }
