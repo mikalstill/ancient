6910ae7a42f80c1bdcb8c975c192aa700cc081a8
u982087 <u982087@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-06-13 10:36:49 +0000

    Added stuff to the interface

--- cep/prototype/cep_gui.m
+++ cep/prototype/cep_gui.m
@@ -39,25 +39,43 @@ gui_handle = figure('Units','pixels','Color',[0.8 0.8 0.8],'Position',frame_pos,
 set(gui_handle,'ToolBar','figure');
 
 % number of buttons
-button_count = 2;
+button_count = 3;
 x_location = 40;
 button_width = 100;
 button_height = 35;
+check_width = 10;
 button_space = (frame_pos(4) - (button_count*button_height))/(button_count+1);
+
 
 % Load button
 load_pos = [x_location, frame_pos(4)-button_space, button_width, button_height];
-load_handle = uicontrol('Parent',gui_handle, 'Callback','cep_callback(''Load'')', 'Units','pixels','Position',load_pos, 'String','Load','Tag','load_button','TooltipString','Load data from file');
+load_handle = uicontrol('Parent', gui_handle, 'Callback', 'cep_callback(''Load'')', ...
+    'Units', 'pixels', 'Position', load_pos, 'String', 'Load', 'Tag', 'load_button', ...
+    'TooltipString', 'Load data from file');
+
+% Referesh button
+refresh_pos = [x_location, frame_pos(4)-((2*button_space)+button_height), button_width, button_height];
+refresh_handle = uicontrol('Parent', gui_handle, 'Callback', 'cep_callback(''Refresh'')', ...
+    'Units', 'pixels', 'Position', refresh_pos, 'String', 'Refresh', 'Tag', 'refresh_button', ...
+    'TooltipString', 'Refresh the Plots');
 
 % Exit button
-exit_pos = [x_location, frame_pos(4)-((2*button_space)+button_height), button_width, button_height];
-exit_handle = uicontrol('Parent',gui_handle, 'Callback','cep_callback(''Exit'')', 'Units','pixels','Position',exit_pos, 'String','Exit',  'Tag','exit_button','TooltipString','Exit the application');
+exit_pos = [x_location, frame_pos(4)-((3*button_space)+button_height), button_width, button_height];
+exit_handle = uicontrol('Parent', gui_handle, 'Callback', 'cep_callback(''Exit'')', ...
+    'Units', 'pixels', 'Position', exit_pos, 'String', 'Exit', 'Tag', 'exit_button', ...
+    'TooltipString', 'Exit the application');
 
+% Error bar check box
+errBar_pos = [x_location, frame_pos(4)-((4*button_space)+button_height), check_width, check_width];
+errBar_handle = uicontrol('Style', 'checkbox', 'Parent', gui_handle, 'String', 'Toggle Error Bars', ...
+    'Units', 'pixels', 'Position', errBar_pos, 'Tag', 'errBar_check', 'TooltipString', ...
+    'Toggle Error Bars');
 
 
 axis_space = 60;
 axis_width  = 2*screen_size(3)/3;
 axis_height = screen_size(4)/4;
+
 
 pos = [border_width + (screen_size(3)/5), top_border_width + (axis_space/3), axis_width, axis_height];
 axis1 = axes('Parent',gui_handle, 'Units','pixels', 'CameraUpVector',[0 1 0], ...
