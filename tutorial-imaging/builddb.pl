06a7ff9b8845d621acc75170b13e372c8f7c149e
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-06-28 23:26:55 +0000

    Mikal: Changing the way command embedding is done to make it funkier

--- tutorial-imaging/builddb.pl
+++ tutorial-imaging/builddb.pl
@@ -1,7 +1,7 @@
 #!/usr/local/bin/perl
 
 use strict;
-my(@args);
+my($cmd, $input, $arg);
 
 # Make sure local scripts can run
 $ENV{PATH} = $ENV{PATH} . ":.";
@@ -9,15 +9,28 @@ $ENV{PATH} = $ENV{PATH} . ":.";
 # This script runs through the specified docbook source, and applies the commands in % blocks
 
 while(<STDIN>){
-    if(/(.*)%([^ \t]*)%(.*)/){
+    if(/(.*)<execute>(.*)<\/execute>(.*)/){
+	print STDERR "Executing $2...\n";
 	print $1;
-	@args = split(/:/, $2);
-	if($args[0] ne "todo"){
-	    print `$args[0] $args[1] < $args[1]`;
+	$cmd = $2;
+	$input = $2;
+	$arg = $2;
+
+	$cmd =~ s/.*<cmd>//;
+	$cmd =~ s/<\/cmd>.*//;
+	$input =~ s/.*<input>//;
+	$input =~ s/<\/input>.*//;
+	$arg =~ s/.*<args>//;
+	$arg =~ s/<\/args>.*//;
+
+	print STDERR "  Cmd: $cmd, Input: $input, Args: $arg\n";
+
+	if($cmd ne "todo"){
+#	    print `$cmd $input < $input`;
 	}
 	else{
 	    print "<figure>\n";
-	    print "<title>$args[1]</title>\n";
+	    print "<title>$input</title>\n";
 	    print "<graphic format=\"EPS\" fileref=\"todo.eps\"></graphic>\n";
 	    print "</figure>\n";
 	}
