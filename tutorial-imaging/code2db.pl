cfbbfd81cfdb191077a3580466ea42390c41f201
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-06-29 09:38:43 +0000

    Mikal: More on the PNG chapter

--- tutorial-imaging/code2db.pl
+++ tutorial-imaging/code2db.pl
@@ -1,19 +1,31 @@
 #!/usr/local/bin/perl
 
 use strict;
+my($suppress);
+
+$suppress = 0;
 
 # Make C code safe for display in docbook
 print "<programlisting>\n";
 
 while(<STDIN>){
     chomp;
-    s/\r$//;
+    if(/^[ \t]*\/\*\*\*\*\*\*\*\*\*/){
+	$suppress = 1;
+    }
+    elsif($suppress == 0){
+	s/\r$//;
+	
+	s/&/&amp;/g;
+	s/</&lt;/g;
+	s/>/&gt;/g;
+	
+	print "$_\n";
+    }
 
-    s/&/&amp;/g;
-    s/</&lt;/g;
-    s/>/&gt;/g;
-
-    print "$_\n";
+    if(($suppress == 1) && (/\*\*\*\*\*\*\*\*\*\/$/)){
+	$suppress = 0;
+    }
 }
 
 print "</programlisting>\n";
