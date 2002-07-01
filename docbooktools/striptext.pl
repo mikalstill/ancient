b18322cf672ae023a2a80892ca2828bad8af726d
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-07-01 20:30:32 +0000

    Mikal: Introduced code to strip out programlistings from source for word counts (if desired)

--- docbooktools/striptext.pl
+++ docbooktools/striptext.pl
@@ -1,13 +1,36 @@
 #!/usr/bin/perl
 
 use strict;
-my($INPUT);
+my($INPUT, $stripcode, $codemode);
+
+$stripcode = 1;
+$codemode = 0;
 
 open INPUT, "< $ARGV[0]";
 while(<INPUT>){
+    if($stripcode == 1){
+	s/.*<programlisting>.*<\/programlisting>//g;
+	if(/<programlisting>.*/){
+	    s/<programlisting>.*//;
+	    $codemode = 1;
+	    
+	    s/<[^<>]*>//g;
+	    s/[^>]*>//g;
+	    s/<[^<]*//g;
+	    print "$_\n";
+	}
+
+	if(/.*<\/programlisting>.*/){
+	    s/.*<\/programlisting>//;
+	    $codemode = 0;
+	}
+    }
+
     s/<[^<>]*>//g;
     s/[^>]*>//g;
     s/<[^<]*//g;
 
-    printf $_;
+    if($codemode == 0){
+	print $_;
+    }
 }
