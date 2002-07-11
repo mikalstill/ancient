9ef3ec1e01d7dababd1499b50cd49f41b5cf2d68
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-07-11 21:10:51 +0000

    Mikal: Working on bug fixes

--- docbooktools/text2db.pl
+++ docbooktools/text2db.pl
@@ -1,36 +1,74 @@
 #!/usr/local/bin/perl
 
 use strict;
-my($temp, $listmode);
+my($line, $prevline, $listmode, $paramode, $ctr_l);
 
 # Make random textfiles safe for display in docbook (outside a programlisting
 # block)
 
-$listmode = "";
+$paramode = 1;
+$listmode = 0;
+
+$ctr_l = chr(12);
+print "<para>";
+
 while(<STDIN>){
+    chomp;
+
     s/&/&amp;/g;
     s/</&lt;/g;
     s/>/&gt;/g;
+    s/\t//g;
+    s/ +/ /g;
+    s/^ //;
 
-    # Wierd characters
-    $temp = chr(12);            # ^L
-    s/$temp/<\/para><para>/;
-    
-    # Blank lines and headings
-    if(/^[ \t]+[0-9A-Za-z][\)\]\.]/){
-	if($listmode eq ""){
-	    print "<orderedlist>\n";
+    $line = $_;
+    $prevline = "NOSUCHLINEFOOFOOFOO";
+
+    while($line ne $prevline){
+	$_ = $line;
+
+	# Forcing a new page
+	if(/$ctr_l/){
+	    if($paramode == 1){
+		$line =~ s/$ctr_l/<\/para><beginpage>/;
+	    }
+	    else{
+		$line =~ s/$ctr_l/<beginpage>/;
+	    }
+	}
+
+	# Blank lines end paragraphs
+	elsif(length($line) < 2){
+	    if($paramode == 1){
+		$line = "</para>";
+		$paramode = 0;
+	    }
+	    if($listmode == 1){
+		$line = "$line</listitem>";
+		$listmode = 0;
+	    }
+	}
+
+	# We deal with lines starting with a number and then a fullstop as a 
+        # list entry (only if we don't have a paragraph open)
+	elsif(($paramode == 0) && (/^[0-9]\./)){
+	    if($listmode == 0){
+		$line = "<itemizedlist>$line";
+	    }
+	    
+	    $line =~  s/[0-9]\./<listitem><para>/;
 	    $listmode = 1;
 	}
-	else{
-	    print "</para></listitem>";
-	}
-	
-	s/[0-9A-Za-z][\)\]\.]/<listitem><para>/;
-    }
-    
-    s/^[ \t]+/<\/para><para>/;
-    s/^[ \t]*$/<\/para><para>/;
 
-    print $_;
+	# If this is a line with text, then we need to start a para sometimes
+	elsif(($paramode == 0) && (/^[^<]/)){
+	    $line = "<para>$line";
+	    $paramode = 1;
+	}
+
+	$prevline = $line;
+    }
+
+    print "$line\n";
 }
