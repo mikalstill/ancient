a748ef437ee006b7e21392a090eca3b0dfdf0614
u983118 <u983118@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-08-07 08:52:14 +0000

    updated cepLs stuff, it works now. Residual calcs are still to come

--- cep/source/cepLs.cpp
+++ cep/source/cepLs.cpp
@@ -1,158 +1,156 @@
 #include "cepLs.h"
-cepLs::cepLs ()
+
+cepLs::cepLs()
 {
 }
 
-cepLs::~cepLs ()
+cepLs::~cepLs()
 {
 }
-void cepLs::cepDoLeastSquares ()
+
+const cepMatrix cepLs::cepDoLeastSquares(cepMatrix &matA, cepMatrix &matP, cepMatrix &matL)
 {
 
-  // this data stuff wil have to go
-  cepMatrix matA, matP, matL, transAP, ans;
-  double a[] = { 1.0, 1.0, 2.0, 1.0, 3.0, 1.0 };
-  double p[] = { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 };
-  double l[] = { 1.0, 1.0, 1.0 };
+  cepMatrix Atrans;
+  cepMatrix AtransP, AtPInv;
+  
+  Atrans = matA;
+  Atrans.transpose();
+  
+  sanityCheck(matA, matP, matL);
+  
 
-  matA.numCols = 2;
-  matA.numRows = 3;
-  matA.matrix = new double[matA.numCols * matA.numRows];
-
-  matA.matrix = a;
-  matP.numCols = 3;
-  matP.numRows = 3;
-  matP.matrix = new double[matP.numCols * matP.numRows];
-
-  matP.matrix = p;
-  matL.numCols = 1;
-  matL.numRows = 3;
-  matL.matrix = new double[matL.numCols * matL.numRows];;
-  matL.matrix = l;
-
-  // make sure dim's of matrix are correct etc.
-  sanityCheck (matA, matP, matL);
-
-  // Do A(T)P
-  if (isDiagonal (matP) == true)
-  {
-    transAP = mulDiag (transpose (matA), matP);
+  if(matP.isDiagonal() == true){
+    AtransP = mulDiag(Atrans, matP);
   }
-
-  else
-  {
+  else{
+    AtransP = Amul(Atrans, matP);
   }
+  
+  AtPInv = mulA(AtransP, matA);
+  AtPInv = inverse(AtPInv);
+  
+  AtransP *= matL;
+  AtPInv *= AtransP;
+  return AtPInv;
 }
-void cepLs::sanityCheck (cepMatrix & matA, cepMatrix & matP, cepMatrix & matL)
+
+const cepMatrix cepLs::Amul(cepMatrix &matA, cepMatrix &matB)
 {
-
-  // matA must be obs x 2
-  if (matP.numCols != matP.numRows)
-  {
-    cout << "Error MatP has wrong dimesions\n";
-    exit (1);
-  }
-
-  // matP must be obs x obs
-  if ((matA.numCols != 2) || (matA.numRows != matP.numRows))
-  {
-    cout << "Error MatA has wrong dimesions\n";
-    exit (1);
-  }
-
-  // matL must be obs x 1
-  if ((matL.numRows != matP.numRows) || (matL.numCols != 1))
-  {
-    cout << "Error MatL has wrong dimesions\n";
-    exit (1);
-  }
-
-  // 2nd col of matA alway = 1
-  for (int i = 0; i < matA.numRows * matA.numCols; i += matA.numCols)
-  {
-    if (matA.matrix[1 + i] != 1)
-    {
-      cout << "Error Mat A has invalid data\n";
-      exit (1);
+  cepMatrix ans(matA.getNumRows(), matB.getNumCols());
+  
+  for(int i = 0; i < ans.getNumRows(); i ++){
+    for(int j = 0; j < ans.getNumCols(); j ++){
+      ans.setValue(i, j, 0);
     }
   }
-}
-
-bool cepLs::isDiagonal (cepMatrix & mat)
-{
-  for (int i = 0; i < mat.numRows; i++)
-  {
-    for (int j = 0; j < mat.numCols; j++)
-    {
-      if ((i != j) && (mat.matrix[i + j * mat.numCols] != 0))
-      {
-        return false;
-      }
+  
+  if(matA.getNumCols() != matB.getNumRows()){
+    cout << "matrix sizes wrong\n";
+    exit(1);
+  }
+  
+  for(i = 0; i < ans.getNumCols(); i ++ ){
+    for(int j = 0; j < matB.getNumCols(); j ++ ){
+      ans.setValue(0, i, (ans.getValue(0, i) + matA.getValue(0,j) * matB.getValue(i,j)));           
+      ans.setValue(1, i, (ans.getValue(1, i) + matB.getValue(i,j)));
     }
-  }
-  return true;
-}
-
-cepMatrix cepLs::transpose (cepMatrix & mat)
-{
-  cepMatrix transpose;
-
-  transpose.numCols = mat.numRows;
-  transpose.numRows = mat.numCols;
-  transpose.matrix = new double[transpose.numCols * transpose.numRows];
-  int x = 0;
-
-  for (int i = 0; i < mat.numCols; i++)
-  {
-    for (int j = 0; j < mat.numRows; j++)
-    {
-      transpose.matrix[x++] = mat.matrix[i + j * mat.numCols];
-    }
-  }
-  return transpose;
-}
-
-//FIX THIS!!
-cepMatrix cepLs::mulDiag (cepMatrix & matA, cepMatrix & matB)
-{
-  cepMatrix ans;
-
-  ans.numRows = matA.numRows;
-  ans.numCols = matB.numCols;
-  ans.matrix = new double[ans.numRows * ans.numCols];
-
-  for (int i = 0; i < matB.numCols; i++)
-  {
-    ans.matrix[i] = matA.matrix[i] * matB.matrix[i + i * matB.numCols];
-    ans.matrix[i + ans.numCols] = matB.matrix[i + i * matA.numCols];
-  }
+  }    
+  
   return ans;
 }
 
-//Will this work???
-cepMatrix cepLs::mul (double *row, int rowElements, cepMatrix & mat)
+const cepMatrix cepLs::mulA(cepMatrix &matA, cepMatrix &matB)
 {
-  return mat;
+  cepMatrix ans(matA.getNumRows(), matB.getNumCols());
+  
+  for(int i = 0; i < ans.getNumRows(); i ++){
+    for(int j = 0; j < ans.getNumCols(); j ++){
+      ans.setValue(i, j, 0);
+    }
+  }
+  
+  if(matA.getNumCols() != matB.getNumRows()){
+    cout << "matrix sizes wrong\n";
+    exit(1);
+  }
+  
+  for(i = 0; i < ans.getNumCols(); i ++ ){
+    for(int j = 0; j < matB.getNumRows(); j ++ ){
+      ans.setValue(i, 0, (ans.getValue(i, 0) + matA.getValue(i,j) * matB.getValue(j,0)));           
+      ans.setValue(i, 1, (ans.getValue(i, 1) + matA.getValue(i,j)));
+    }
+  }    
+  
+  return ans;
 }
 
-cepMatrix cepLs::inverse (cepMatrix & mat)
+const cepMatrix cepLs::mulDiag(cepMatrix &matA, cepMatrix &matB)
 {
-  cepMatrix ans;
+  cepMatrix ans(matA.getNumRows(), matB.getNumCols());
+  
+  if(matA.getNumCols() != matB.getNumRows()){
+    cout << "matrix sizes wrong\n";
+    exit(1);
+  }
+
+  for(int i = 0; i < ans.getNumRows(); i ++){
+    for(int j = 0; j < ans.getNumCols(); j ++){
+      ans.setValue(i, j, matA.getValue(i, j) * matB.getValue(j,j));
+    }
+  }
+  
+  return ans;
+}
+
+const cepMatrix cepLs::inverse(cepMatrix &mat)
+{
+  cepMatrix ans(mat.getNumRows(), mat.getNumCols());
+  ans = mat;
+
   double mul;
-
-  ans.numRows = mat.numCols;
-  ans.numCols = mat.numCols;
-  ans.matrix = new double[ans.numRows * ans.numCols];
-
-  if ((mat.numCols == 2) && (mat.numRows == 2))
-  {
+  
+  if ( ( mat.getNumCols() != 2 ) || ( mat.getNumRows() != 2 ) ){
     cout << "error in matrix inverse - invalid matrix size\n";
-    exit (1);
+    exit(1);
   }
-  mul = 1 / (mat.matrix[0] * mat.matrix[3] - mat.matrix[1] * mat.matrix[2]);
-  ans.matrix[0] = mat.matrix[3] * mul;
-  ans.matrix[1] = -mat.matrix[1] * mul;
-  ans.matrix[2] = -mat.matrix[2] * mul;
-  ans.matrix[3] = mat.matrix[0] * mul;
+  
+  mul = 1/( mat.getValue(0, 0) * mat.getValue(1, 1) - mat.getValue(0, 1) * mat.getValue(1, 0) );
+  
+  ans.setValue(0,0, mat.getValue(1,1) * mul);
+  ans.setValue(0,1, -mat.getValue(0,1) * mul);
+  ans.setValue(1,0, -mat.getValue(1,0) * mul);
+  ans.setValue(1,1, mat.getValue(0,0) * mul);
+  
   return ans;
 }
+
+void cepLs::sanityCheck( cepMatrix &matA, cepMatrix &matP, cepMatrix &matL)
+{
+  //matA must be obs x 2
+  if( matP.getNumCols() != matP.getNumRows() ){
+    cout << "Error MatP has wrong dimesions\n";
+    exit(1);
+  }
+  
+  //matP must be obs x obs
+  if( ( matA.getNumCols() !=2 ) || ( matA.getNumRows() != matP.getNumRows() ) ){
+    cout << "Error MatA has wrong dimesions\n";
+    exit(1);
+  }
+  
+  //matL must be obs x 1
+  if( ( matL.getNumRows() != matP.getNumRows() ) || (matL.getNumCols() != 1 ) ){
+    cout << "Error MatL has wrong dimesions\n";
+    exit(1);
+  }
+
+  //2nd col of matA alway = 1
+  for(int i = 0; i < matA.getNumRows(); i ++){
+    if (matA.getValue(i, matA.getNumCols()-1) != 1 ){
+      cout << "Error Mat A has invalid data\n";
+      exit(1);
+    }
+  }
+}
+
