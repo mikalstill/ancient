74df1dc78f70086a3afa867a612992cb7652e780
u964076 <u964076@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-08-10 12:13:00 +0000

    Mikal: Added a sample dialog box, the eliminate outlyers user interface

---
+++ cep/source/cepEliminateDialog.cpp
@@ -0,0 +1,41 @@
+
+/* 
+   Imp for the CEP data presentation
+   Copyright (C) Michael Still                    2002
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cepCore.h"
+#include "cepEliminateDialog.h"
+
+// We define simple things like the size of the dialog here, the dialog
+// is actually displayed using display() below...
+cepEliminateDialog::cepEliminateDialog():
+  m_position(0, 0),
+  m_size(200, 200),
+  m_dialog(NULL, -1, "Eliinate outliers", m_position, m_size, 
+	   wxDIALOG_MODAL, "eliminateOutliers"),
+  m_posok(10, 10),
+  m_ok(m_dialog, wxID_OK, "Ok", m_posok)
+{
+}
+
+void cepEliminateDialog::display()
+{
+  m_dialog.Centre();
+  m_dialog.ShowModal();
+  
+}
---
+++ cep/source/cepEliminateDialog.h
@@ -0,0 +1,36 @@
+/* 
+   Imp for the CEP data presentation
+   Copyright (C) Michael Still                    2002
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "wx/docmdi.h"
+#include <wx/image.h>
+#include <wx/dialog.h>
+#include <wx/button.h>
+#include "cepCore.h"
+
+class cepEliminateDialog{
+ public:
+  cepEliminateDialog();
+  void display();
+
+ private:
+  wxPoint m_position, mposok;
+  wxSize m_size;
+  wxDialog m_dialog;
+  wxButton m_ok;
+};
