f34822c32d1af034e592823941aacf3702962ad2
u964076 <u964076@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-08-11 00:20:56 +0000

    Mikal: Corrected copyright statement, and improved error display config key

--- cep/source/cepWxErrorHandler.cpp
+++ cep/source/cepWxErrorHandler.cpp
@@ -1,19 +1,22 @@
-/***************************************************************************
-                          cepwxerrohandler.cpp  -  description
-                             -------------------
-    begin                : Fri Aug 9 2002
-    copyright            : (C) 2002 by Blake Swadling
-    email                : blake@swadling.org
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/* 
+   Imp for the CEP dataset
+   Copyright (C) Michael Still                    2002
+   Copyright (C) Blake Swadling                   2002
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #include "cepWxErrorHandler.h"
 
@@ -45,7 +48,7 @@ void cepWxErrorHandler::displayError( class cepError & err )
     // Deliberately dropping cepError return value here
     bool dodisp = false;
 
-    config->getValue (string ("cepErrordisplaylevel") +
+    config->getValue (string ("error-display-") +
                  cepItoa ((int)level), true, dodisp);
 
     gDisplayParams[(int)level].set (dodisp);
