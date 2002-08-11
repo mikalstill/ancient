a6739f08c7c285990d9ed202548100a94f79b9df
u964076 <u964076@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-08-11 01:22:25 +0000

    Mikal: Finished standardizing the copyright statements

--- cep/source/cepUI.cpp
+++ cep/source/cepUI.cpp
@@ -1,35 +1,35 @@
 
 /* 
-   UI for the CEP program, replaces the cepMain.cpp file
-   Copyright (C) Michael Still                    2002
-
-   Based on a demo which originally had this notice:
-
-   ////////////////////////////////////////////////////////////////////////////
-   // Name:        docview.cpp
-   // Purpose:     Document/view demo
-   // Author:      Julian Smart
-   // Created:     04/01/98
-   // Copyright:   (c) Julian Smart and Markus Holzem
-   // Licence:   	wxWindows license
-   ////////////////////////////////////////////////////////////////////////////
-  
-   [The wxWindows license is compatible with the GNU GPL, as it is the GNU
-   LGPL]
-   
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   UI for the CEP program, replaces the cepMain.cpp file
+ *   Copyright (C) Michael Still                    2002
+ *
+ *   Based on a demo which originally had this notice:
+ *
+ *   /////////////////////////////////////////////////////////////////////////
+ *   // Name:        docview.cpp
+ *   // Purpose:     Document/view demo
+ *   // Author:      Julian Smart
+ *   // Created:     04/01/98
+ *   // Copyright:   (c) Julian Smart and Markus Holzem
+ *   // Licence:   	wxWindows license
+ *   /////////////////////////////////////////////////////////////////////////
+ *  
+ *   [The wxWindows license is compatible with the GNU GPL, as it is the GNU
+ *   LGPL]
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *   
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *   
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #define GLOBALS_HERE
