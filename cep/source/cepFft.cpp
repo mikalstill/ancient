f822d1ec4b9230daee9fc94a19745d71f3dd538a
u991672 <u991672@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-08-24 05:00:24 +0000

    fixed cepFft to correctly split real and imaginary numbers.

--- cep/source/cepFft.cpp
+++ cep/source/cepFft.cpp
@@ -21,23 +21,28 @@
 #include "cfft.h"
 #include <stdio.h>
 #include <iostream.h>
+#include <stdlib.h>
 
 
-//topdo: daniel. The results from cepFft.cpp miss those of octave by a factor of 32..
-//seems to be because the transforms are arse about..i.e fft is actually ifft.
-int
-main (void)
+//todo: generate error when argument is not power of 2.
+//what if int too small.
+int main (int argc, char *argv[])
 {
 
-  int arraySize = 32;
+  int arraySize = atoi(argv[1]);
+  cout << "type def....";
   typedef complex <double>Complex;
+
+  cout << "<Complex.......";
 
   cfft<Complex> FFT32 (arraySize);   // build an operator object
   // The constructor builds tables and places them in the object.
 
+
+  cout << "<Complex array.......";
   Complex Array[arraySize];
 
-  //fill the array ...
+
   for (int k = 0; k < arraySize; k++)
   {
     Array[k] = sin (k);
@@ -48,20 +53,19 @@ main (void)
   //do the transform
   FFT32.fft (Array);  // forward transform
   cout << "\n";
-
+  double re[arraySize];
+  double im[arraySize];
   //debugging output..
   for (int k = 0; k < arraySize; k++)
-	{
-    cout << "Post transform: Array[" << k << "]: " << Array[k] << "\n";
-    //the scaling factor of 32 - test make answer right...debugging...interesting??!
-    //cout << "Post transform: Array[" << k << "]: " << ((Array[k])*32) << "\n";
+  {
+    cout << "Post transform: Array[" << k << "]: real = " << real(Array[k]) << "  imag = " << imag(Array[k]) << "\n";
   }
 
   cout << "\n";
   //reverse the transform to check if it's even close to working
-  FFT32.fft (Array);            // reverse transform.
+  FFT32.ifft (Array);            // reverse transform.
 
   //debugging output..
   for (int k = 0; k < arraySize; k++)
-     cout << "Post inverse transform: Array[" << k << "]: " << Array[k] << "\n";
+     cout << "Post inverse transform: Array[" << k << "]: real: " << real(Array[k]) << "  imag = " << imag(Array[k]) << "\n";
 }//end main
