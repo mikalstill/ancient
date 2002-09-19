3426bca8ab0a040d55c5e07a13b024a76e280caa
u964076 <u964076@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-09-19 01:34:15 +0000

    Mikal: UI tests -- part of the testallhuge target

--- cep/source/tests/cep_testPresentation_buildall.sh
+++ cep/source/tests/cep_testPresentation_buildall.sh
@@ -2,8 +2,11 @@
 
 # I had to do this, because otherwise GCC never exits with code files this
 # large (at least on my little laptop)
-./cep_testPresentation_build.sh 0 1000 cep_testPresentationOne.cpp
-./cep_testPresentation_build.sh 1001 2000 cep_testPresentationTwo.cpp
-./cep_testPresentation_build.sh 2001 3000 cep_testPresentationThree.cpp
-./cep_testPresentation_build.sh 3001 4000 cep_testPresentationFour.cpp
-./cep_testPresentation_build.sh 4001 5000 cep_testPresentationFive.cpp
+count=0
+jumpsize=200
+
+while [ $count -lt 4500 ]
+do
+  ./cep_testPresentation_build.sh $count $(( $count + $jumpsize - 1 )) cep_testPresentation`printf "%03d" $count`.cpp
+  count=$(( $count + $jumpsize ))
+done
