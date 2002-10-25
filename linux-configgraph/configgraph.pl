7b114f8444081a2b673e1daa5548fd8ec8153098
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-10-25 05:43:25 +0000

    Mikal: This doesn't work, but it is getting a lot closer to having a tree of dependancies happening...

--- linux-configgraph/configgraph.pl
+++ linux-configgraph/configgraph.pl
@@ -1,133 +1,184 @@
 #!/usr/bin/perl
 
 use strict;
-my($depth, $condition, $temp);
+my($depth, $condition, $temp, $hashref);
 
 $depth = 0;
-$condition = "NONE";
-while(<STDIN>){
-    if(/^\#/){
-	# ...comment...
-    }
-    elsif(/^[ \t]*$/){
-	# Blank lines...
-    }
+$condition = "";
 
-    ##########
-    # Menus    
-    elsif(/^[ \t]*mainmenu_name[ \t](.*)/){
-	depthIndent();
-	print getString($1)."<BR>\n";
-	$depth = 1;
-    }
-    elsif(/^[ \t]*mainmenu_option next_comment/){
-	$depth++;
-	print "<BR>\n";
-    }
-    elsif(/^[ \t]*comment[ \t](.*)/){
-	depthIndent();
-	print getString($1)."<BR>\n";
-	$depth++;
-    }
-    elsif(/^[ \t]*endmenu/){
-	$depth -= 2;
-    }
+$hashref = process("$ARGV[0]/arch/i386/config.in", $hashref);
+dumphash($hashref, 0);
 
-    ##########
-    # Booleans    
-    elsif(/^[ \t]*define_bool[ \t]+(.*)[ \t]+(.*)/){
-	depthIndent();
-	markCondition();
-	if($2 eq "y"){
-	    print "<FONT COLOR=\"green\">";
+
+
+
+
+
+
+
+
+
+###########################################################################
+
+sub process(){
+    my($filename, $hashref) = @_;
+    local(*INPUT);
+
+    open INPUT, "< $filename";
+    while(<INPUT>){
+	if(/^[ \t]*\#/){
+	    # ...comment...
 	}
+	elsif(/^[ \t]*$/){
+	    # Blank lines...
+	}
+	
+	##########
+	# Menus -- we display these to give an idea of parsing progress
+	elsif(/^[ \t]*mainmenu_name[ \t](.+)/){
+#	    print STDERR depthIndent().getString($1)."\n";
+	    $depth = 1;
+	}
+	elsif(/^[ \t]*mainmenu_option next_comment/){
+	    $depth++;
+	}
+	elsif(/^[ \t]*comment[ \t](.+)/){
+#	    print depthIndent().getString($1)."\n";
+	    $depth++;
+	}
+	elsif(/^[ \t]*endmenu/){
+	    $depth -= 2;
+	}
+	
+	##########
+	# Booleans    
+	elsif(/^[ \t]*bool[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "bool\t$condition\t$2\ty,n\t".getString($1));
+	}
+	elsif(/^[ \t]*dep_bool[ \t]+(.+)[ \t]+(.+)[ \t]+(.+)/){
+	    #todo condition();
+#	    print "Dependant boolean: ".getString($1)." $2 on $3\n";
+	}
+	elsif(/^[ \t]*define_bool[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "define_bool\t$condition\t$1\t$2");
+	}
+
+	#########
+	# Mbooleans
+	elsif(/^[ \t]*dep_mbool[ \t]+(.+)[ \t]+(.+)[ \t]+(.+)/){
+	    #todo condition();
+#	    print "Dependant mboolean: ".getString($1)." $2 on $3\n";
+	}
+	
+	#########
+	# Ints
+	elsif(/^[ \t]*int[ \t]+(.+)[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "int\t$condition\t$2\t$3,anyint\t".getString($1));
+	}
+	elsif(/^[ \t]*dep_int[ \t]+(.+)[ \t]+(.+)/){
+	    #todo condition();
+#	    print "$1 = $2\n";
+	}
+	elsif(/^[ \t]*define_int[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "define_int\t$condition\t$1\t$2");
+	}
+
+	#########
+	# Hex
+	elsif(/^[ \t]*hex[ \t]+(.+)[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "hex\t$condition\t$2\t$3,anyhex\t".getString($1));
+	}
+	elsif(/^[ \t]*dep_hex[ \t]+(.+)[ \t]+(.+)/){
+	    #todo condition();
+#	    print "$1 = $2\n";
+	}
+	elsif(/^[ \t]*define_hex[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "define_hex\t$condition\t$1\t$2");
+	}
+
+	#########
+	# Strings
+	elsif(/^[ \t]*string[ \t]+(.+)[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "string\t$condition\t$2\t$3,anystring\t".getString($1));
+	}
+	elsif(/^[ \t]*dep_string[ \t]+(.+)[ \t]+(.+)/){
+	    #todo condition();
+#	    print "$1 = $2\n";
+	}
+	elsif(/^[ \t]*define_string[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "define_string\t$condition\t$1\t$2");
+	}
+	
+	#########
+	# Tristates
+	elsif(/^[ \t]*tristate[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "tristate\t$condition\t$2\ty,n,m\t".getString($1));
+	}
+	elsif(/^[ \t]*dep_tristate[ \t]+(.+)[ \t]+(.+)[ \t]+(.+)/){
+	    #todo condition();
+#	    print "Dependant tristate: ".getString($1)." $2 on $3\n";
+	}
+	elsif(/^[ \t]*define_tristate[ \t]+(.+)[ \t]+(.+)/){
+	    $hashref = addstatement($hashref, "define_tristate\t$condition\t$1\t$2");
+	}
+	
+	##########
+	# Conditionals
+	elsif(/^[ \t]*if \[ \"(.+)\" ([!=]+) \"(.+)\" \][ \t]*;/){
+	    if($condition ne ""){
+		$condition = "$condition;$1 $2 $3";
+	    }
+	    else{
+		$condition = "$1 $2 $3";
+	    }
+	}
+	elsif(/^[ \t]*fi/){
+	    $condition =~ s/;[^;]*$//;
+	}
+	elsif(/^[ \t]*else/){
+	    $condition =~ s/;[^;]*$//;
+	    $condition = "$condition;else";
+	}
+	
+	##########
+	# Process other files
+	elsif(/^[ \t]*source[ \t](.+)/){
+#	    print "\t\t\t[Sourced file: $ARGV[0]/$1]\n";
+	    process("$ARGV[0]/$1");
+#	    print "\t\t\t[End sourced file: $ARGV[0]/$1]\n";
+	}
+	
+	##########
+	# Choices
+	elsif(/^[ \t]*choice[ \t](.+)[ \t]+\\/){
+	    #todo condition();
+#	    print "Choice: ".getString($1)."\n";
+	}
+	elsif(/[ \t]+[\"]?([^ \t]*)[ \t]+(.+)[ \t]+(\\)/){
+	    foreach $temp (split/\//, $1){
+		#todo condition();
+#	    print "$temp: $2\n";	    
+	    }
+	}
+	elsif(/[ \t]+([^ \t]*)[ \t]+(.+)\"[ \t]+(.+)/){
+	    foreach $temp (split/\//, $1){
+		#todo condition();
+#		print "$temp: $2\n";	    
+	    }
+	    #todo condition();
+#	    print "Default: $3\n";
+	}
+	
+	##########
+	# Unknown lines
 	else{
-	    print "<FONT COLOR=\"red\">";
-	}
-	print "$1 = $2</FONT><BR>\n";
-    }
-    elsif(/^[ \t]*bool[ \t]+(.*)[ \t]+(.*)/){
-	depthIndent();
-	markCondition();
-	print getString($1)." ($2)<BR>\n";
-    }
-    elsif(/^[ \t]+dep_bool[ \t]+(.*)[ \t]+(.*)[ \t]+(.*)/){
-	depthIndent();
-	markCondition();
-	print "Dependant boolean: ".getString($1)." $2 on $3<BR>\n";
-    }
-
-    elsif(/^[ \t]*define_int[ \t]+(.*)[ \t]+(.*)/){
-	depthIndent();
-	markCondition();
-	print "$1 = $2<BR>\n";
-    }
-
-    elsif(/^[ \t]*tristate[ \t]+(.*)[ \t]+(.*)/){
-	depthIndent();
-	markCondition();
-	print "tristate: ".getString($1)." ($2)<BR>\n";
-    }
-    elsif(/^[ \t]*dep_tristate[ \t]+(.*)[ \t]+(.*)[ \t]+(.*)/){
-	depthIndent();
-	markCondition();
-	print "Dependant tristate: ".getString($1)." $2 on $3<BR>\n";
-    }
-
-    ##########
-    # Conditionals
-    elsif(/^[ \t]*if \[ \"(.*)\" ([!=]+) \"(.*)\" \][ \t]*;/){
-	$condition = "$condition;$1 $2 $3";
-    }
-    elsif(/^[ \t]*fi/){
-	$condition =~ s/;[^;]*$//;
-    }
-    elsif(/^else/){
-	$condition =~ s/;[^;]*$//;
-	$condition = "$condition;else";
-    }
-
-    ##########
-    # Process other files
-    elsif(/^[ \t]*source[ \t](.*)/){
-	# todo
-    }
-
-    ##########
-    # Choices
-    elsif(/^[ \t]*choice[ \t](.*)[ \t]+\\/){
-	depthIndent();
-	markCondition();
-	print "Choice: ".getString($1)."<BR>\n";
-	$depth++;
-    }
-    elsif(/[ \t]+[\"]?([^ \t]*)[ \t]+(.*)[ \t]+(\\)/){
-	foreach $temp (split/\//, $1){
-	    depthIndent();
-	    markCondition();
-	    print "$temp: $2<BR>\n";	    
+#	    print STDERR "Unknown line: $_";
 	}
     }
-    elsif(/[ \t]+([^ \t]*)[ \t]+(.*)\"[ \t]+(.*)/){
-	foreach $temp (split/\//, $1){
-	    depthIndent();
-	    markCondition();
-	    print "$temp: $2<BR>\n";	    
-	}
-	depthIndent();
-	markCondition();
-	print "<B>Default: $3</B><BR><BR>\n";
-	$depth--;
-    }
 
-    ##########
-    # Unknown lines
-    else{
-	print STDERR "Unknown line: $_";
-    }
+    return $hashref;
 }
 
-##############################################################################
 sub getString(){
     my($input) = @_;
     my($temp);
@@ -144,21 +195,60 @@ sub getString(){
 	return $temp;
     }
     else{
-	print STDERR "Unknown string format: $input\n";
+#	print STDERR "Unknown string format: $input\n";
 	return $input;
     }
 }
 
 sub depthIndent(){
-    my($count);
+    my($count, $string);
 
+    $string = "";
     for($count; $count < $depth; $count++){
-	print "&nbsp;&nbsp;&nbsp;&nbsp;";
+	$string = "$string ";
     }
+    return $string;
 }
 
-sub markCondition(){
-    if($condition ne ""){
-	print "<FONT COLOR=\"orange\">$condition: </FONT>";
+sub addstatement(){
+    my($hash_ref, $string) = @_;
+    my(@cols, @conditions, $newstring);
+
+    print "Adding $string\n";
+
+    # Break the line into it's columns
+    @cols = split(/\t/, $string);
+    @conditions = split(/;/, @cols[1]);
+
+    if($conditions[0] eq ""){
+	$hash_ref->{"END"} = "$string!".$hashref->{"END"};
+	print "TERMINAL: ".$hash_ref->{"END"}."\n\n\n\n";
+    }
+    else{
+	splice(@conditions, 0, 1);
+	$newstring = "$cols[0]\t".join(/;/, @conditions)."\t$cols[2]\t$cols[3]\t$cols[4]";
+	$hash_ref->{$conditions[0]} = addstatement($hash_ref->{$conditions[0]}, $newstring);
+    }
+
+    return $hash_ref;
+}
+
+sub dumphash()
+{
+    my($hash_ref, $indent) = @_;
+    my($key, $count);
+
+    foreach $key (keys %$hash_ref){
+        for($count = 0; $count < $indent; $count++){
+            print " ";
+        }
+
+	print "[$key]\n";
+        if($key ne "END"){
+            dumphash($hash_ref->{$key}, $indent + 1);
+        }
+	else{
+	    print $hash_ref->{$key}."\n";
+	}
     }
 }
