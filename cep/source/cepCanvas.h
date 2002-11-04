1baae80aea93382f71ffe6be9cb3f3f94822de4b
u964076 <u964076@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-11-04 22:26:54 +0000

    Mikal: Added the source code appendix to the technical manual

--- cep/source/cepCanvas.h
+++ cep/source/cepCanvas.h
@@ -40,6 +40,33 @@
 #include "cepDataset.h"
 #include "cepUI.h"
 
+/******************************************************************************
+DOCBOOK START
+
+FUNCTION cepCanvas
+PURPOSE owns all drawing to user visible areas
+
+SYNOPSIS START
+#include&lt;cepCanvas.h&gt;
+
+SYNOPSIS END
+
+DESCRIPTION Set the starting point for the sequence of curves and lines that it to be drawn on the current page. This call is compulsory for almost all of the line drawing functions. It is not required for the <command>panda_rectangle</command> call.
+
+EXAMPLE START
+#include&lt;libplot.h&gt;
+plot_state *graph;
+
+if((graph = plot_newplot(400, 300)) == NULL){
+  ... error ...
+}
+
+EXAMPLE END
+SEEALSO 
+DOCBOOK END
+******************************************************************************/
+
+
 class cepCanvas:public wxScrolledWindow
 {
 public:
