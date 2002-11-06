f11499f304fb7b42760cb8f1bff6ccde7a5ac45d
u983118 <u983118@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-11-06 07:08:30 +0000

    tests for new cepDate class

--- cep/source/tests/cep_testDate.cpp
+++ cep/source/tests/cep_testDate.cpp
@@ -76,13 +76,13 @@ protected:
   /** date test 1. Tests a simple date set */
   void dateTest1 ()
   { 
-    cepDate date(2002.0027);
+    cepDate date(2002.0000);
     string year("2002"), 
            month("01"), 
            day("01"), 
            shortDate("01/01/2002"), 
            longDate("01 January 2002");
-        
+            
     CPPUNIT_ASSERT_MESSAGE ("wrong Year", year == date.getYear());
     CPPUNIT_ASSERT_MESSAGE ("wrong Month", month == date.getMonth());
     CPPUNIT_ASSERT_MESSAGE ("wrong Day", day == date.getDay());
@@ -94,13 +94,13 @@ protected:
   /** date test 2. Tests a simple date */
   void dateTest2 ()
   { 
-    cepDate date(2002.9855);
+    cepDate date(2002.9999);
     string year("2002"), 
            month("12"), 
            day("31"), 
            shortDate("31/12/2002"), 
            longDate("31 December 2002");
-   
+
     CPPUNIT_ASSERT_MESSAGE ("wrong Year", year == date.getYear());
     CPPUNIT_ASSERT_MESSAGE ("wrong Month", month == date.getMonth());
     CPPUNIT_ASSERT_MESSAGE ("wrong Day", day == date.getDay());
@@ -112,7 +112,7 @@ protected:
   /** date test 3. Tests a simple date */  
   void dateTest3 ()
   { 
-    cepDate date(1973.3996);
+    cepDate date(1973.4047);
     string year("1973"), 
            month("05"), 
            day("28"), 
@@ -130,27 +130,26 @@ protected:
   /** date leapYear. Tests for a leap year */  
   void leapYear ()
   { 
-    cepDate date(2000.1620);
+    cepDate date(2000.1639);
     string year("2000"), 
            month("02"), 
            day("29"), 
            shortDate("29/02/2000"), 
            longDate("29 February 2000");
-
+   
     CPPUNIT_ASSERT_MESSAGE ("wrong Year", year == date.getYear());
     CPPUNIT_ASSERT_MESSAGE ("wrong Month", month == date.getMonth());
     CPPUNIT_ASSERT_MESSAGE ("wrong Day", day == date.getDay());
     CPPUNIT_ASSERT_MESSAGE ("wrong Short Date", shortDate == date.getShortDate());
     CPPUNIT_ASSERT_MESSAGE ("wrong Long Date", longDate == date.getLongDate());
     
-    cepDate date2(2004.1620);
+    cepDate date2(2004.1639);
     string year2("2004"), 
            month2("02"), 
            day2("29"), 
            shortDate2("29/02/2004"), 
            longDate2("29 February 2004");
-    
-   
+
     CPPUNIT_ASSERT_MESSAGE ("wrong Year", year2 == date2.getYear());
     CPPUNIT_ASSERT_MESSAGE ("wrong Month", month2 == date2.getMonth());
     CPPUNIT_ASSERT_MESSAGE ("wrong Day", day2 == date2.getDay());
@@ -162,7 +161,7 @@ protected:
   /** date nonleapYear. Tests to make sure leap years working */  
   void nonLeapYear ()
   { 
-    cepDate date(1999.1620);
+    cepDate date(1999.1643);
     string year("1999"), 
            month("03"), 
            day("01"), 
@@ -175,7 +174,7 @@ protected:
     CPPUNIT_ASSERT_MESSAGE ("wrong Short Date", shortDate == date.getShortDate());
     CPPUNIT_ASSERT_MESSAGE ("wrong Long Date", longDate == date.getLongDate());
     
-    cepDate date2(3000.1620);
+    cepDate date2(3000.1643);
     string year2("3000"), 
            month2("03"), 
            day2("01"), 
