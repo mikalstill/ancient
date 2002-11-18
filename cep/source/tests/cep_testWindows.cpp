90b920edddae9637ba591cbdda568944821775ef
u982087 <u982087@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-11-18 04:59:33 +0000

    updated test and updated result sheets

--- cep/source/tests/cep_testWindows.cpp
+++ cep/source/tests/cep_testWindows.cpp
@@ -27,7 +27,7 @@
  * Tests the framework which has been set up to window the incoming data
  *
  * @author Blake Swadling
- * @version $Revision: 1.12 $
+ * @version $Revision: 1.13 $
  */
 
 namespace {
@@ -91,18 +91,29 @@ protected:
   {
 //    cout << "testing hamming ... " << endl;
     int size = 100;
-    cepDataWindower::setWindowType( cepDataWindower::WINDOW_HAMMING, size, 0 );
+    int overlap = 50;
+    int tables = 6;
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_HAMMING, size, overlap );
     
     cepMatrix<double> result;
-    cepMatrix<double> input = makeData( size );
+    cepMatrix<double> input = makeData( size*(tables+1)/2 );
     cepError err = cepDataWindower::window( input , result );
     if( err.isReal() ) {
       err.display();
     }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
 
     ofstream f("hamming.txt", ios::trunc);
+    f << endl;
     for( int i=0; i<result.getNumRows(); ++i ) {
-      f << result.getValue(i, 0, 0) << ' ' << result.getValue(i, 1, 0) << endl;
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
     }
     f.close();
 
@@ -116,18 +127,28 @@ protected:
   {
 //    cout << "testing hanning ... " << endl;
     int size = 100;
-    cepDataWindower::setWindowType( cepDataWindower::WINDOW_HANNING, size, 0 );
+    int overlap = 50;
+    int tables = 6;
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_HANNING, size, overlap );
     
     cepMatrix<double> result;
-    cepMatrix<double> input = makeData( size );
+    cepMatrix<double> input = makeData( size*(tables+1)/2 );
     cepError err = cepDataWindower::window( input , result );
     if( err.isReal() ) {
       err.display();
     }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
 
     ofstream f("hanning.txt", ios::trunc);
     for( int i=0; i<result.getNumRows(); ++i ) {
-      f << result.getValue(i, 0, 0) << ' ' << result.getValue(i, 1, 0) << endl;
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
     }
     f.close();
 
@@ -140,23 +161,32 @@ protected:
   {
 //    cout << "testing blackman ... " << endl;
     int size = 100;
-    cepDataWindower::setWindowType( cepDataWindower::WINDOW_BLACKMAN, size, 0 );
+    int overlap = 50;
+    int tables = 6;
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_BLACKMAN, size, overlap );
 
     cepMatrix<double> result;
-    cepMatrix<double> input = makeData( size );
+    cepMatrix<double> input = makeData( size*(tables+1)/2 );
     cepError err = cepDataWindower::window( input , result );
     if( err.isReal() ) {
       err.display();
     }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
 
     ofstream f("blackman.txt", ios::trunc);
     for( int i=0; i<result.getNumRows(); ++i ) {
-      f << result.getValue(i, 0, 0) << ' ' << result.getValue(i, 1, 0) << endl;
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
     }
     f.close();
 
   }
-
 
 
 
@@ -166,18 +196,65 @@ protected:
   void testChebyshev ()
   {
 //    cout << "testing chebyshev ... " << endl;
-    int size = 99;
-    cepDataWindower::setWindowType( cepDataWindower::WINDOW_CHEBYSHEV, size, 0 );
+    int size = 100;
+    int overlap = 0;
+    int tables = 1;
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_CHEBYSHEV, size, overlap );
+    cepDataWindower::setChebBandwidth( 0.01 );
     cepMatrix<double> result;
     cepMatrix<double> input = makeData( size );
     cepError err = cepDataWindower::window( input , result );
     if( err.isReal() ) {
       err.display();
-    } 
+    }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
 
     ofstream f("chebyshev.txt", ios::trunc);
     for( int i=0; i<result.getNumRows(); ++i ) {
-      f << result.getValue(i, 0, 0) << ' ' << result.getValue(i, 1, 0) << endl;
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
+    }
+    f.close();
+
+  }
+
+
+
+  /** Test the hamming window. Dumps the coefficients to a delimited text file
+   * for viewing in your spread sheet of choice
+   */
+  void testChebyshev1 ()
+  {
+//    cout << "testing chebyshev ... " << endl;
+    int size = 100;
+    int overlap = 50;
+    int tables = 6;
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_CHEBYSHEV, size, overlap );
+    cepDataWindower::setChebBandwidth( 0.02 );
+    cepMatrix<double> result;
+    cepMatrix<double> input = makeData( size*(tables+1)/2 );
+    cepError err = cepDataWindower::window( input , result );
+    if( err.isReal() ) {
+      err.display();
+    } 
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
+
+    ofstream f("chebyshev.txt", ios::app);
+    for( int i=0; i<result.getNumRows(); ++i ) {
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
     }
     f.close();
 
@@ -186,19 +263,58 @@ protected:
   void testChebyshev2 ()
   {
 //    cout << "testing chebyshev ... " << endl;
-    int size = 100;
-    cepDataWindower::setWindowType( cepDataWindower::WINDOW_CHEBYSHEV, size, 0 );
+    int size = 99;
+    int overlap = 50;
+    int tables = 6;    
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_CHEBYSHEV, size, overlap );
+    cepDataWindower::setChebBandwidth( 0.03 );
     cepMatrix<double> result;
-    cepMatrix<double> input = makeData( size );
+    cepMatrix<double> input = makeData( size*(tables+1)/2 );
     cepError err = cepDataWindower::window( input , result );
     if( err.isReal() ) {
       err.display();
     }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
 
     ofstream f("chebyshev.txt", ios::app);
     f << endl << endl;
     for( int i=0; i<result.getNumRows(); ++i ) {
-      f << result.getValue(i, 0, 0) << ' ' << result.getValue(i, 1, 0) << endl;
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
+    }
+    f.close();
+
+  }
+
+  void testMultipleWindows () {
+    int size=512;
+    int overlap=256;
+    int tables=3;
+    cepDataWindower::setWindowType( cepDataWindower::WINDOW_CHEBYSHEV, size, overlap );
+    cepDataWindower::setChebBandwidth( 0.05 );
+    cepMatrix<double> result;
+    cepMatrix<double> input = makeData( size*(tables+1)/2 );
+    cepError err = cepDataWindower::window( input , result );
+    if( err.isReal() ) {
+      err.display();
+    }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of windows" , tables, result.getNumTables() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "incorrect number of elements", size,   result.getNumRows() );
+
+    ofstream f("multiple.txt", ios::trunc);
+    for( int i=0; i<result.getNumRows(); ++i ) {
+      f << result.getValue(i, 0, 0);
+      for( int j=0; j<result.getNumTables(); ++j ) {
+        CPPUNIT_ASSERT_MESSAGE( "value is NAN", !isnan(result.getValue(i, 1, j)) );
+        f << '\t' << result.getValue(i, 1, j);
+      }
+      f << endl;
     }
     f.close();
 
@@ -209,11 +325,12 @@ private:
   // makes a data set with linear scale (1->n) and data of all 1's
   const cepMatrix<double> & makeData( int size ) {
     
-    cepMatrix<double> *data = new cepMatrix<double>( size, 2 );
+    cepMatrix<double> *data = new cepMatrix<double>( size, 3 );
     
     for( int i=0; i<size; ++i ) {
       data->setValue(i, 0, i);
       data->setValue(i, 1, 1);
+      data->setValue(i, 2, 0);
     }
     return *data;
     
@@ -256,7 +373,12 @@ public:
     suiteOfTests->addTest(
       new CppUnit::TestCaller<Test>( "testChebyshev", &Test::testChebyshev ) );
     suiteOfTests->addTest(
+      new CppUnit::TestCaller<Test>( "testChebyshev2", &Test::testChebyshev1 ) );
+    suiteOfTests->addTest(
       new CppUnit::TestCaller<Test>( "testChebyshev2", &Test::testChebyshev2 ) );
+    suiteOfTests->addTest(
+      new CppUnit::TestCaller<Test>( "testMultipleWindows", &Test::testMultipleWindows ) );
+    
     
        
     return suiteOfTests;
