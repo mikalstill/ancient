d986b41023ab5c837c4299d82b3811b1c613e75b
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-12-22 10:50:25 +0000

    Mikal: Highlighting different lines with color, and some internal reworking...

--- side/side.c
+++ side/side.c
@@ -18,15 +18,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define COLWIDTH 70
+#define LINEWIDTH 140
+
+int side_min(int, int);
 
 int main(int argc, char *argv[]){
-  char temp1[] = "/tmp/side.XXXXXX", temp2[] = "/tmp/side.XXXXXX", fmt[255], cmd1[1024], cmd2[1024],
-    *file1, *file2;
-  int fd1, fd2, cnt1, cnt2, done;
-  struct stat sb1, sb2;
-  off_t inset1, inset2;
-  int count1, count2, currfile;
+  char temp[2][100], fmt[255], cmd[2][1024], *file[2], color[2][10], line[LINEWIDTH];
+  int fd[2], cnt[2], done;
+  struct stat sb[2];
+  off_t inset[2];
+  int count[2], currfile;
 
   // Check the arguements
   if(argc < 4){
@@ -34,130 +35,99 @@ int main(int argc, char *argv[]){
     exit(1);
   }
 
-  // Filter the files into temporary locations -- open to format string attacks
+  // Setup the color control codes. This should be integrated with getopt one day...
+  snprintf(color[0], 10, "%c[34m", 27); // blue
+  snprintf(color[1], 10, "%c[31m", 27); // red
+
+  // Filter the files into temporary locations -- open to format string attacks, as
+  // we as temporary file substitution attacks
   fprintf(stderr, "Warning, do not use this command in a secure environment...\n");
-
-  if(fd1 = mkstemp(temp1) < 0){
-    perror("Could not make temporary file one");
-    exit(1);
-  }
-  close(fd1);
   snprintf(fmt, 255, "%s > %%s", argv[1]);
-  snprintf(cmd1, 1024, fmt, argv[2], temp1);
-  system(cmd1);
-  if(fd1 = open(temp1, NULL) < 0){
-    perror("Error opening temporary file one");
-    exit(1);
-  }
-
-  if(fd2 = mkstemp(temp2) < 0){
-    perror("Could not make temporary file two");
-    exit(1);
-  }
-  close(fd2);
-  snprintf(cmd2, 1024, fmt, argv[2], temp2);
-  system(cmd2);
-  if(fd2 = open(temp2, NULL) < 0){
-    perror("Error opening temporary file two");
-    exit(1);
-  }
-
-  // Do some memory mapping
-  if(fstat(fd1, &sb1) < 0){
-    perror("Could not stat file one");
-    exit(1);
-  }
-  if(fstat(fd2, &sb2) < 0){
-    perror("Could not stat file two");
-    exit(1);
-  }
-
-  if ((file1 =
-       (char *) mmap (NULL, sb1.st_size, PROT_READ, MAP_SHARED, fd1, 0)) == -1)
-    {
-      perror("Could not mmap file one");
-      exit (1);
+  for(currfile = 0; currfile < 2; currfile++){
+    snprintf(temp[currfile], 100, "/tmp/side.XXXXXX");
+    if(fd[currfile] = mkstemp(temp[currfile]) < 0){
+      perror("Could not make temporary file one");
+      exit(1);
+    }
+    close(fd[currfile]);
+    snprintf(cmd[currfile], 1024, fmt, argv[2 + currfile], temp[currfile]);
+    system(cmd[currfile]);
+    if(fd[currfile] = open(temp[currfile], NULL) < 0){
+      perror("Error opening temporary file one");
+      exit(1);
     }
 
-  if ((file2 =
-       (char *) mmap (NULL, sb2.st_size, PROT_READ, MAP_SHARED, fd2, 0)) == -1)
-    {
-      perror("Could not mmap file two");
-      exit (1);
+    // Do some memory mapping
+    if(fstat(fd[currfile], &sb[currfile]) < 0){
+      perror("Could not stat file");
+      exit(1);
     }
 
-  // We now have filtered files we can display
-  printf("%-70s    | %s\n", cmd1, cmd2);
-  inset1 = inset2 = 0;
-  count1 = count2 = 1;
-  currfile = 0;
-  while((inset1 < sb1.st_size) && (inset2 < sb2.st_size)){
-    if(currfile == 0){
-      if(file1[inset1] == '\t') count1 += 7;
-      
-      if(count1 > COLWIDTH){
-	printf(" \\\\ | ");
-	count1 = 1;
-	currfile++;
+    if ((file[currfile] =
+	 (char *) mmap (NULL, sb[currfile].st_size, PROT_READ, MAP_SHARED, fd[currfile], 0)) == -1)
+      {
+	perror("Could not mmap file");
+	exit (1);
       }
-      
-      if(file1[inset1] == '\n'){
-	for(; count1< COLWIDTH + 1; count1++)
-	  printf(" ");
-	printf("    | ");
-	count1 = 0;
-	currfile++;
+
+    count[currfile] = 0;
+    inset[currfile] = 0;
+  }
+
+  // Display the files
+  // Color control codes stolen from http://www.termsys.demon.co.uk/vtansi.htm
+  memset(line, ' ', LINEWIDTH);
+  snprintf(line, LINEWIDTH / 2, "%s", cmd[0]);
+  line[side_min(strlen(cmd[0]), LINEWIDTH / 2 - 1)] = ' ';
+  snprintf(line + (LINEWIDTH / 2) + 1, LINEWIDTH / 2, "%s", cmd[1]);
+  line[LINEWIDTH / 2] = '|';
+  printf("  %s\n", line);
+
+  memset(line, '-', LINEWIDTH);
+  line[LINEWIDTH] = '\0';
+  line[LINEWIDTH - 1] = '\n';
+  line[LINEWIDTH / 2] = '|';
+  printf("  %s", line);
+
+  while((inset[0] < sb[0].st_size) || (inset[1] < sb[1].st_size)){
+    memset(line, ' ', LINEWIDTH);
+    line[LINEWIDTH] = '\0';
+    line[LINEWIDTH - 1] = '\n';
+    line[LINEWIDTH / 2] = '|';
+
+    for(currfile = 0; currfile < 2; currfile++){
+      if(inset[currfile] >= sb[currfile].st_size){
+	line[1 + (currfile * LINEWIDTH / 2)] = '*';
       }
       else{
-	printf("%c", file1[inset1]);
+	while((count[currfile]++ < (LINEWIDTH / 2) - 1) && (file[currfile][inset[currfile]] != '\n')){
+	  line[count[currfile] + (currfile * LINEWIDTH / 2) + currfile] = file[currfile][inset[currfile]++];
+	  if(file[currfile][inset[currfile] - 1] == '\t') count[currfile] += 7;
+	}      
       }
-      
-      inset1++;
-      count1++;
-    }
-    else{
-      if(file2[inset2] == '\t') count2 += 7;
 
-      if(count2 > COLWIDTH){
-	printf(" \\\\\n");
-	count2 = 0;
-	currfile--;
-      }
-      
-      if(file2[inset2] == '\n'){
-	printf("\n");
-	count2 = 1;
-	currfile--;
-      }
-      else{
-	printf("%c", file2[inset2]);
-      }
-      
-      inset2++;
-      count2++;
+      count[currfile] = 0;
+      inset[currfile]++;
     }
+
+    if(strncmp(line, line + (LINEWIDTH / 2) + 1, (LINEWIDTH / 2) - 2) == 0)
+      printf("%s  ", color[0]);
+    else printf("%s++", color[1]);
+    printf("%s", line);
   }
-  
-  // Get to a new line and display end of file markers
-  if((currfile == 1) || (count1 > 0)) printf("\n");
-  if(inset1 > sb1.st_size - 1) printf("%-70s    | ", " [EOF]");
-  else printf("%-70s    | ", " ");
 
-  if(inset2 > sb2.st_size - 1) printf("%s", " [EOF]");
-  printf("\n\n");
-  
   // Cleanup
-  if(munmap(file1, sb1.st_size) < 0){
-    perror("Could not unmap memory one");
-    exit(1);
+  printf("%c[0m", 27);
+  for(currfile = 0; currfile < 2; currfile++){
+    if(munmap(file[currfile], sb[currfile].st_size) < 0){
+      perror("Could not unmap memory one");
+      exit(1);
+    } 
+    close(fd[currfile]);
   }
-
-  if(munmap(file2, sb2.st_size) < 0){
-    perror("Could not unmap memory two");
-    exit(1);
-  }
-  
-  close(fd1);
-  close(fd2);
 }
-  
+
+int side_min(int a, int b){
+  if(a < b) return a;
+  return b;
+}
