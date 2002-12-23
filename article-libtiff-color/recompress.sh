e349352df5ed6a74abb25180afcf23592cfdc0bd
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2002-12-23 04:11:43 +0000

    Mikal: Updated

--- article-libtiff-color/recompress.sh
+++ article-libtiff-color/recompress.sh
@@ -5,6 +5,7 @@ cp $1 $1-0.tif
 
 while [ $count -lt 200 ]
 do
-  ./read $1-$(( $count -1 )).tif $1-$count.tif
+  echo tiffcp -c jpeg $1-$(( $count -1 )).tif $1-$count.tif
+  ./recompress $1-$(( $count -1 )).tif $1-$count.tif
   count=$(( $count + 1 ))
 done
