e733a6045aff6c5423798f3266899ca1b8348f91
root <root@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2003-01-28 06:06:31 +0000

    This commit was generated by cvs2svn to compensate for changes in r2367, which included commits to RCS files with non-trunk default branches.

---
+++ rsync-backup/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+logger "rsync-backup $$ Starting"
+for item in `find / -xdev -type f -name ".BACKUP" -print`
+do
+  echo "Seen $item"
+  ldir=`echo $item | sed 's/.BACKUP//'`
+  rdir=`cat $item`
+  logger "rsync-backup $$ Local is $ldir"
+  logger "rsync-backup $$ Remote is $rdir"
+
+  logger "rsync-backup $$ Starting rsync"
+  date >> $item-LOG
+  rsync --exclude=.BACKUP --one-file-system --progress -rcv $ldir/. $rdir/ >> $item-LOG
+  logger "rsync-backup $$ Sync returned $?"
+done
+logger "rsync-backup $$ Finished"
