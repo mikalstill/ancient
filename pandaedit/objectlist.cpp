ec88c57b47ce9ccf15295c1a17ebf2265bc7d6fa
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2003-02-04 22:01:40 +0000

    Mikal: Implemented page list recursion

--- pandaedit/objectlist.cpp
+++ pandaedit/objectlist.cpp
@@ -21,23 +21,53 @@ objectlist::objectlist (string input, pdf* thePDF):
 	  input.substr (1, input.length () - 2));
   while (1)
     {
-      if (!isPositiveInteger (tokens[inset]))
+      if (!isPositiveInteger (tokens[inset])){
+	debug(dlError, "Object number for page is not a positive integer");
 	return;
-      if (!isPositiveInteger (tokens[inset + 1]))
+      }
+      if (!isPositiveInteger (tokens[inset + 1])){
+	debug(dlError, "Generation number for page is not a positive integer");
 	return;
-      if (tokens[inset + 2] != "R")
+      }
+      if (tokens[inset + 2] != "R"){
+	debug(dlError, "Object reference for page lacks a reference identifier");
 	return;
+      }
 
+      // This can be a recursive list, so we check that here
       ref.number = atoi (tokens[inset++].c_str ());
       ref.generation = atoi (tokens[inset++].c_str ());
-      inset++;
+      object subpages(-1, -1);
+      if(!thePDF->findObject(ref.number, ref.generation, subpages)){
+	debug(dlError, "Referenced page does not exist");
+	return;
+      }
 
-      debug(dlTrace, string("List referenced object ") + toString(ref.number) +
-	    string(" ") + toString(ref.generation));
-      m_objects.push_back (ref);
+      string subkids;
+      if(subpages.getDict().getValue("Kids", subkids)){
+	debug(dlTrace, "This pages entry is recursive");
+
+	objectlist pages(subkids, thePDF);
+	for(unsigned int p = 0; p < pages.size(); p++){
+	  ref.number = pages[p].getNumber();
+	  ref.generation = pages[p].getGeneration();
+	  m_objects.push_back(ref);
+	}
+
+	debug(dlTrace, string("Recursion found ") + toString(pages.size()) +
+	      string(" elements"));
+      }
+      else{
+	debug(dlTrace, string("List referenced object ") + toString(ref.number) +
+	      string(" ") + toString(ref.generation));
+	m_objects.push_back (ref);
+      }
+
+      inset++;
     }
 
-  debug(dlTrace, string("Built a ") + toString(m_objects.size()) + string(" item list"));
+  debug(dlTrace, string("Built a ") + toString(m_objects.size()) + 
+	string(" item list"));
 }
 
 object
