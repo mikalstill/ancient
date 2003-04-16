7595504ba4a7d90a231000d056a9cb8cc49daf4f
root <root@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2003-04-16 12:20:04 +0000

    Mikal: Mega patch to send to Dave Jones

--- x86info-wx/makepatch.sh
+++ x86info-wx/makepatch.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Make a release copy of the new code
+rm -rf wx-release
 cp -R wx wx-release
 cd wx-release
 rm -f config.status config.log config.cache Makefile
 rm -rf .deps
+find . -type d -name "CVS" -exec rm -rf {} \;
+cd ..
+
+# Make a test copy of Dave Jone's code
+rm -rf x86info-1.11-release
+cp -R x86info-1.11 x86info-1.11-release
+cd x86info-1.11-release
 find . -type d -name "CVS" -exec rm -rf {} \;
