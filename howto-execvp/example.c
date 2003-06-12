5d48a920318a52e37d13e3fc80dbb05d99677e99
root <root@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2003-06-12 04:29:17 +0000

    This commit was generated by cvs2svn to compensate for changes in r2646, which included commits to RCS files with non-trunk default branches.

---
+++ howto-execvp/example.c
@@ -0,0 +1,27 @@
+// Demonstrates how to use execvp
+//
+// Copyright (c) Michael Still 2003, released under the terms of the GNU GPL
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  char **args;
+
+  // Args is an array of char *'s which point to the arguements. The first
+  // arg is traditionally the name of the command
+  if((args = (char **) malloc(5 * sizeof(char *))) == NULL){
+    printf("Couldn't allocate memory\n");
+    exit(1);
+  }
+
+  args[0] = strdup("echo");
+  args[1] = strdup("hello");
+  args[2] = strdup("out");
+  args[3] = strdup("there");
+  args[4] = NULL;
+
+  execvp("echo", args);
+  printf("Could not execvp()\n");
+  exit(1);
+}
