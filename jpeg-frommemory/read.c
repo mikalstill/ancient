b17cbd02df6ed540e54c5415f7b026e2a567d8c8
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2004-07-31 09:13:29 +0000

    Mikal: Random commit

--- jpeg-frommemory/read.c
+++ jpeg-frommemory/read.c
@@ -6,15 +6,69 @@
 #include <jpeglib.h>
 #include <tiffio.h>
 
-typedef struct {
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char *file;
+off_t size;
+
+typedef struct
+{
   struct jpeg_source_mgr pub;   /* public fields */
 
-  FILE * infile;                /* source stream */
   JOCTET * buffer;              /* start of buffer */
   boolean start_of_file;        /* have we gotten any data yet? */
-} my_source_mgr;
+} *src_ptr;
 
-typedef my_source_mgr * my_src_ptr;
+static boolean
+fill_input_buffer(cinfo)
+    j_decompress_ptr cinfo;
+{
+  src_ptr src = (src_ptr) cinfo->src;
+
+  if (size <= 0) {
+    /* Insert a fake EOI marker */
+    src->buffer[0] = (JOCTET) 0xFF;
+    src->buffer[1] = (JOCTET) JPEG_EOI;
+    size = 2;
+  }
+  else
+    {
+      src->buffer = file;
+    }
+
+  src->pub.next_input_byte = src->buffer;
+  src->pub.bytes_in_buffer = size;
+
+  return TRUE;
+}
+
+static void
+skip_input_data(cinfo, num_bytes)
+    j_decompress_ptr cinfo;
+    long num_bytes;
+{
+  src_ptr src = (src_ptr) cinfo->src;
+
+  if (num_bytes > 0) {
+    while (num_bytes > (long) src->pub.bytes_in_buffer) {
+      num_bytes -= (long) src->pub.bytes_in_buffer;
+      fill_input_buffer(cinfo);
+    }
+    src->pub.next_input_byte += (size_t) num_bytes;
+    src->pub.bytes_in_buffer -= (size_t) num_bytes;
+  }
+}
+
+static void
+dummy_source(cinfo)
+    j_decompress_ptr cinfo;
+{
+  /* no work necessary here */
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,11 +77,11 @@ int main(int argc, char *argv[])
   FILE * infile;
   char *img, *upto;
   JSAMPARRAY buffer;
+  src_ptr src;
 
   TIFF *output;
   
   int fd;
-  char *file;
   struct stat sb;
 
   /* Map the input file into memory */
@@ -41,13 +95,29 @@ int main(int argc, char *argv[])
     perror("Could not stat file");
     exit(43);
   }
+  size = sb.st_size;
 
   if ((file =
-       (char *) mmap (NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0)) == -1)
+       (char *) mmap (NULL, size, PROT_READ, MAP_SHARED, fd, 0)) == -1)
     {
       perror("Could not mmap file");
       exit (43);
     }
+
+  /* Setup the JPEG memory reading stuff */
+  src = (src_ptr)
+    (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
+				sizeof(src_ptr));
+  cinfo->src = (struct jpeg_source_mgr *) src;
+
+  src->pub.init_source = dummy_source;
+  src->pub.fill_input_buffer = fill_input_buffer;
+  src->pub.skip_input_data = skip_input_data;
+  src->pub.resync_to_restart = jpeg_resync_to_restart; /* use default method */
+  src->pub.term_source = dummy_source;
+
+  src->pub.bytes_in_buffer = 0; /* forces fill_input_buffer on first read */
+  src->pub.next_input_byte = NULL; /* until buffer loaded */
 
   /* Do JPEG things */
   cinfo.err = jpeg_std_error(&jerr);
