33c4e895f6eaa10a656ed01e2b4e61c2ba4982af
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2008-12-05 01:18:11 +0000

    Mikal: Tweak release script

--- makerelease.sh
+++ makerelease.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Do a generic release, including building a tarball
 # $1 is the package to release
 # $2 is the version number
 
-svn copy $1 ../branches/$1-release-$2
+#svn copy $1 ../branches/$1-release-$2
+#cd ..
+#svn commit --message "Release script: Branching $1 for release $2"
 
-# More stuff here
+mkdir /tmp/$$
+cd /tmp/$$
+svn co http://www.stillhq.com/svn/branches/$1-release-$2
+mv $1-release-$2 $1-$2
+tar cvzf $1-$2.tgz $1-$2
+
+echo "Tarball in /tmp/$$"
