1ab6bcc872d00ae52829f513083526e7fae2a14b
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2009-03-08 18:54:56 +0000

    Mikal: Refactor

--- mp3server/server.py
+++ mp3server/server.py
@@ -35,6 +35,14 @@ h.close()
 
 
 requests = {}
+bytes = 0
+
+_CONTENT_TYPES = {'css':  'text/css',
+                  'html': 'text/html',
+                  'mp3':  'audo/x-mpeg-3',
+                  'png':  'image/png',
+                  'swf':  'application/x-shockwave-flash'
+                 }
 
 
 class http_server(asyncore.dispatcher):
@@ -96,168 +104,182 @@ class http_handler(asyncore.dispatcher):
       print '%s %s GET %s' %(datetime.datetime.now(), repr(self.addr),
                              file)
     if file == '/':
-      self.buffer += 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
-
-      for row in self.db.GetRows('select paths from tracks '
-                                 'where paths <> "[]" '
-                                 'order by rand() limit 10;'):
-        paths = eval(row['paths'])
-        this_track = track.Track(self.db)
-        this_track.FromPath(paths[0])
-        rendered = this_track.RenderValues()
-
-        if os.path.exists(rendered['mp3_path']):
-          if self.addr[0] in requests:
-            print '%s %s Marking %s skipped' %(datetime.datetime.now(),
-                                               repr(self.addr),
-                                               requests[self.addr[0]])
-            self.db.ExecuteSql('update tracks set skips = skips + 1, '
-                               'last_skipped = NOW(), last_action = NOW() '
-                               'where id=%s;'
-                               % requests[self.addr[0]])
-            self.db.ExecuteSql('commit;')
-
-            del requests[self.addr[0]]
-
-          requests[self.addr[0]] = rendered['id']
-
-          play_graph = {}
-          skip_graph = {}
-          for row in self.db.GetRows('select song, plays, skips, last_action, '
-                                     '(time_to_sec(now()) - '
-                                     ' time_to_sec(last_played)) as lp, '
-                                     '(time_to_sec(now()) - '
-                                     ' time_to_sec(last_skipped)) as ls, '
-                                     '(time_to_sec(now()) - '
-                                     ' time_to_sec(last_action)) as secs '
-                                     'from tracks '
-                                     'where last_action is not null and '
-                                     '(time_to_sec(now()) - '
-                                     ' time_to_sec(last_action)) < 3600 '
-                                     'order by last_action asc;'):
-
-            if row['lp']:
-              play_graph.setdefault(int(row['lp'] / 60), 0)
-              play_graph[int(row['lp'] / 60)] += 1
-
-            if row['ls']:
-              skip_graph.setdefault(int(row['ls'] / 60), 0)
-              skip_graph[int(row['ls'] / 60)] += 1
-
-          play = ''
-          skip = ''
-          for i in range(60):
-            play += '%d,' % play_graph.get(i, 0)
-            skip += '%d,' % skip_graph.get(i, 0)
-          
-          rendered['graph'] = ('http://chart.apis.google.com/chart?cht=bvg&'
-                               'chbh=a&chds=0,10,0,10&chs=800x125&chd=t:%s|%s&'
-                               'chco=00FF00,FF0000'
-                               %(play[:-1], skip[:-1]))
-
-          
-          
-          self.buffer += _TEMPLATE % rendered
-          break
-
-        else:
-          print '%s %s Missing MP3 %s' %(datetime.datetime.now(),
-                                         repr(self.addr), paths[0])
-          if self.addr[0] in requests:
-            del requests[self.addr[0]]
-
+      self.handleurl_root()
     elif file.startswith('/mp3/'):
-      self.buffer += 'HTTP/1.1 200 OK\r\nContent-Type: audio/x-mpeg-3\r\n\r\n'
-      for row in self.db.GetRows('select paths from tracks '
-                                 'where id=%s;'
-                                 % file[5:]):
-        paths = eval(row['paths'])
-        for path in paths:
-          if path.endswith('.mp3') and os.path.exists(path):
-            print '%s %s Serving %s' %(datetime.datetime.now(),
-                                       repr(self.addr), path)
-            f = open(path)
-            self.buffer += f.read(1000000000)
-            f.close()
-            break
-
-    elif file.startswith('/local/player'):
-      self.buffer += ('HTTP/1.1 200 OK\r\n'
-                      'Content-Type: application/x-shockwave-flash \r\n\r\n')
-      try:
-        f = open('player_mp3_js.swf')
-        self.buffer += f.read(1000000000)
-        f.close()
-
-      except Exception, e:
-        print '%s %d player serving error: %s' %(datetime.datetime.now(),
-                                                 repr(self.addr), e)
-
-    elif file.startswith('/local/style.css'):
-      self.buffer += ('HTTP/1.1 200 OK\r\n'
-                      'Content-Type: text/css \r\n\r\n')
-      try:
-        f = open('style.css')
-        self.buffer += f.read(1000000000)
-        f.close()
-
-      except Exception, e:
-        print '%s %s css serving error: %s' %(datetime.datetime.now(),
-                                              repr(self.addr), e)
-
-    elif file.startswith('/local/control_'):
-      self.buffer += ('HTTP/1.1 200 OK\r\n'
-                      'Content-Type: image/png \r\n\r\n')
-      try:
-        f = open(file.split('/')[-1])
-        self.buffer += f.read(1000000000)
-        f.close()
-
-      except Exception, e:
-        print '%s %s image serving error: %s' %(datetime.datetime.now(),
-                                                repr(self.addr), e)
-
+      self.handleurl_mp3(file)
+    elif file.startswith('/local/'):
+      self.handleurl_local(file)
     elif file.startswith('/done'):
-      self.buffer += ('HTTP/1.1 200 OK\r\n'
-                      'Content-Type: text/html \r\n\r\n'
-                      '<html><head><title>...</title></head>'
-                      '<body></body></html>')
-
-      id = file.split('/')[-1]
-      print '%s %s Marking %s played' %(datetime.datetime.now(),
-                                        repr(self.addr), id)
-      self.db.ExecuteSql('update tracks set plays = plays + 1, '
-                         'last_played = NOW(), last_action = NOW() '
-                         'where id=%s;'
-                         % id)
-      self.db.ExecuteSql('commit;')
-
-      if self.addr[0] in requests:
-        del requests[self.addr[0]]
-
+      self.handleurl_done(file)
     else:
-      self.buffer += ('HTTP/1.1 404 OK\r\nContent-Type: text/html\r\n\r\n'
-                      '<html><head><title>MP3 server</title></head>'
-                      '<body>File not found</body>')
+      self.senderror(404, '%s file not found' % file)
 
   def writable(self):
     return len(self.buffer) > 0
 
   def handle_write(self):
+    global bytes
+    
     try:
       sent = self.send(self.buffer)
+      bytes += sent
+
       self.buffer = self.buffer[sent:]
       if len(self.buffer) == 0:
         self.close()
+
     except:
       pass
 
   def handle_close(self):
     pass
 
+  def handleurl_root(self):
+    """The top level of the user interface."""
+    
+    self.buffer += 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+
+    for row in self.db.GetRows('select paths from tracks '
+                               'where paths <> "[]" '
+                               'order by rand() limit 10;'):
+      paths = eval(row['paths'])
+      this_track = track.Track(self.db)
+      this_track.FromPath(paths[0])
+      rendered = this_track.RenderValues()
+
+      if os.path.exists(rendered['mp3_path']):
+        if self.addr[0] in requests:
+          print '%s %s Marking %s skipped' %(datetime.datetime.now(),
+                                             repr(self.addr),
+                                             requests[self.addr[0]])
+          self.db.ExecuteSql('update tracks set skips = skips + 1, '
+                             'last_skipped = NOW(), last_action = NOW() '
+                             'where id=%s;'
+                             % requests[self.addr[0]])
+          self.db.ExecuteSql('commit;')
+
+          del requests[self.addr[0]]
+
+        requests[self.addr[0]] = rendered['id']
+
+        play_graph = {}
+        skip_graph = {}
+        for row in self.db.GetRows('select song, plays, skips, last_action, '
+                                   '(time_to_sec(now()) - '
+                                   ' time_to_sec(last_played)) as lp, '
+                                   '(time_to_sec(now()) - '
+                                   ' time_to_sec(last_skipped)) as ls, '
+                                   '(time_to_sec(now()) - '
+                                   ' time_to_sec(last_action)) as secs '
+                                   'from tracks '
+                                   'where last_action is not null and '
+                                   '(time_to_sec(now()) - '
+                                   ' time_to_sec(last_action)) < 3600 '
+                                   'order by last_action asc;'):
+
+          if row['lp']:
+            play_graph.setdefault(int(row['lp'] / 60), 0)
+            play_graph[int(row['lp'] / 60)] += 1
+
+          if row['ls']:
+            skip_graph.setdefault(int(row['ls'] / 60), 0)
+            skip_graph[int(row['ls'] / 60)] += 1
+
+        play = ''
+        skip = ''
+        for i in range(60):
+          play += '%d,' % play_graph.get(i, 0)
+          skip += '%d,' % skip_graph.get(i, 0)
+
+        rendered['graph'] = ('http://chart.apis.google.com/chart?cht=bvg&'
+                             'chbh=a&chds=0,10,0,10&chs=800x125&chd=t:%s|%s&'
+                             'chco=00FF00,FF0000'
+                             %(play[:-1], skip[:-1]))
+
+
+
+        self.buffer += _TEMPLATE % rendered
+        break
+
+      else:
+        print '%s %s Missing MP3 %s' %(datetime.datetime.now(),
+                                       repr(self.addr), paths[0])
+        if self.addr[0] in requests:
+          del requests[self.addr[0]]
+
+  def handleurl_done(self, file):
+    """Mark an MP3 as played."""
+
+    self.buffer += ('HTTP/1.1 200 OK\r\n'
+                    'Content-Type: text/html \r\n\r\n'
+                    '<html><head><title>...</title></head>'
+                    '<body></body></html>')
+
+    id = file.split('/')[-1]
+    print '%s %s Marking %s played' %(datetime.datetime.now(),
+                                      repr(self.addr), id)
+    self.db.ExecuteSql('update tracks set plays = plays + 1, '
+                       'last_played = NOW(), last_action = NOW() '
+                       'where id=%s;'
+                       % id)
+    self.db.ExecuteSql('commit;')
+
+    if self.addr[0] in requests:
+      del requests[self.addr[0]]
+
+  def handleurl_mp3(self, file):
+    """Serve MP3 files."""
+
+    for row in self.db.GetRows('select paths from tracks '
+                               'where id=%s;'
+                               % file[5:]):
+      paths = eval(row['paths'])
+      for path in paths:
+        if path.endswith('.mp3') and os.path.exists(path):
+          self.sendfile(path)
+          return
+
+    self.senderror(500, 'MP3 %s missing' % file)
+
+  def handleurl_local(self, file):
+    """Return a local file needed by the user interface."""
+
+    ent = file.split('/')[-1]
+    self.sendfile(ent)
+                    
+  def sendfile(self, path):
+    """Send a file to the client, including doing the MIME type properly."""
+
+    data = ''
+    try:
+      f = open(path)
+      data += f.read(1000000000)
+      f.close()
+    except:
+      senderror(404, 'File read error: %s' % path)
+      return
+
+    extn = path.split('.')[-1]
+    self.buffer += ('HTTP/1.1 200 OK\r\n'
+                    'Content-Type: %s\r\n'
+                    'Content-Length: %s\r\n\r\n'
+                    '%s'
+                    %(_CONTENT_TYPES.get(extn, 'application/octet-stream'),
+                      len(data), data))
+    print '%s %s Sent %s (%d bytes)' %(datetime.datetime.now(),
+                                       repr(self.addr),
+                                       path, len(data))
+
+  def senderror(self, number, msg):
+    self.buffer += ('HTTP/1.1 %d OK\r\nContent-Type: text/html\r\n\r\n'
+                    '<html><head><title>MP3 server</title></head>'
+                    '<body>%s</body>'
+                    %(number, msg))
+
 
 def main(argv):
   global running
+  global bytes
   
   # Parse flags
   try:
@@ -272,8 +294,14 @@ def main(argv):
   print '%s Started listening on port %s' %(datetime.datetime.now(),
                                             FLAGS.port)
 
+  last_summary = datetime.datetime.now()
   while running:
     asyncore.loop(timeout=10.0, count=1)
+
+    delta = datetime.datetime.now() - last_summary
+    if delta.seconds > 60:
+      print '%s TOTAL BYTES SERVED: %d' %(datetime.datetime.now(), bytes)
+      last_summary = datetime.datetime.now()
 
 
 if __name__ == "__main__":
