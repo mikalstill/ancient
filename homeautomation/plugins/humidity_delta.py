9998e375546a78498c45ca1e195eb58156c484ca
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2010-01-24 10:58:01 +0000

    Use new humidity data to work out the inside / outside delta

---
+++ homeautomation/plugins/humidity_delta.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# Work out how much more humid it is in the house compared with outside
+
+def Requires(unused_cursor, unused_sensor_names):
+  """What inputs are required?"""
+  return ['=Inside humidity', '=Outside humidity']
+
+
+def Returns(unused_cursor):
+  """What values would be returned?"""
+  return ['=Humidity delta']
+
+
+def Calculate(inputs, redirects, log=None):
+  """Determine delta."""
+
+  out = []
+    
+  while inputs['=Inside humidity']:
+    if inputs['=Inside humidity'][0] and inputs['=Outside humidity'][0]:
+      try:
+        inside = int(inputs['=Inside humidity'][0])
+        outside = int(inputs['=Outside humidity'][0])
+        out.append(inside - outside)
+        
+      except Exception, e:
+        if log:
+          log('Int parse error: %s' % e)
+        out.append(None)
+
+    else:
+      out.append(None)
+
+    for input in inputs:
+      inputs[input] = inputs[input][1:]
+
+  return out
