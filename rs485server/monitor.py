7637369043377fc48ca6bb3bd2220deb2186c34d
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2011-01-20 04:03:37 +0000

    Random commit

--- rs485server/monitor.py
+++ rs485server/monitor.py
@@ -2,20 +2,33 @@
 
 import datetime
 import serial
+import string
 import sys
 import time
 
 s = serial.Serial(rtscts=True, xonxoff=False)
 s.port = sys.argv[1]
-s.baudrate = 115200
+s.baudrate = 9600 
 s.open()
 
 input = ''
+sys.stdout.write('>> ')
+
 while True:
   if s.inWaiting() > 0:
-    input += s.read()
+    d = s.read()
+    input += d
+
+    for c in d:
+      if c in string.printable and not c in ['\r', '\n']:
+        sys.stdout.write('%d (%s) ' %(ord(c), c))
+      else:
+        sys.stdout.write('%d ' % ord(c))
+    sys.stdout.flush()
+
     if input.endswith('\n'):
       if len(input) > 2:
-        sys.stdout.write('%s: %s' %(datetime.datetime.now(), input))
+        sys.stdout.write('\n%s: %s' %(datetime.datetime.now(), input))
+        sys.stdout.write('>> ')
         sys.stdout.flush()
       input = ''
