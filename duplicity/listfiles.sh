0432505d0843324eed801fb29393f0cbaf46a232
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2011-10-19 04:59:55 +0000

    Load the results of backups into a SQL database

--- duplicity/listfiles.sh
+++ duplicity/listfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Helper for listfiles
-# $1 is the age of the backup set
+# $1 is the date to query
 # $2 is the location of the backup set
 
 source /data/backups/secrets
@@ -9,5 +9,5 @@ source /data/backups/secrets
 /data/src/duplicity/duplicity-bin \
     --encrypt-key=${GPG_KEY} \
     --sign-key=${GPG_KEY} \
-    --time $1"D" \
+    --time $1 \
     list-current-files file://$2
