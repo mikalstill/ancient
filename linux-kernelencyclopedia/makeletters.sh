fc7649ec314d3ecb2775785d7fa868de566c33fe
mikal <mikal@e7f0f05e-9a5c-0410-9775-f07f60351c7c>
2012-05-24 06:05:25 +0000

    Delete obsolete code

--- linux-kernelencyclopedia/makeletters.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for item in a b c d e f g h i j k l m n o p q r s t u v w x y z
-do
-  export LETTER=$item
-  subst < templateletter.sgml > letter-$item.sgml
-done
--- mfc-speech/c++/encodeall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for item in `ls s*.mfc`
-do
-  ./encode codebook.mfc $item `echo $item | sed 's/mfc$/enc/'`
-done
